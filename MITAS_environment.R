1f554e40fe2d766344d251414e18a1c07757ee55
Nicole Swartwood <nswartwo@gmail.com>
2018-01-18 11:01:55 -0500

    Add files via upload

---
+++ MITAS_environment.R
@@ -0,0 +1,8 @@
+#' The MITAS Variable Environment
+#'
+#' @name MITAS_env
+#'
+#' @description This is the environment where variables and parameters necessary
+#' for the MITAS model will be stored.
+#'
+#'
