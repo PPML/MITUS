41b2282b5dab308d3b78024774c031eb3b7d3efb
nswartwo <nswartwo@gmail.com>
2018-02-23 09:51:31 -0500

    moves definition of P into its own script

---
+++ R/define_P.R
@@ -0,0 +1,12 @@
+###this script is to move this code into a place that I can find
+### it until I can decide the best place to move it into for final
+### model run
+
+load("ParamInitUS_V738tab.rData") # ParamInit
+P  <- ParamInit[,1];
+names(P) <- rownames(ParamInit)
+ii <-  ParamInit[,5]==1
+ParamInitZ <- ParamInit[ParamInit$Calib==1,]
+idZ0 <- ParamInitZ[,4]==0
+idZ1 <- ParamInitZ[,4]==1
+idZ2 <- ParamInitZ[,4]==2
