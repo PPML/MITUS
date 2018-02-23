4b527a031f7b641cfcb693f8e4ad875af6ccfc2c
nswartwo <nswartwo@gmail.com>
2018-02-23 10:00:37 -0500

    updates the file path for data read in

--- R/define_P.R
+++ R/define_P.R
@@ -2,7 +2,7 @@
 ### it until I can decide the best place to move it into for final
 ### model run
 
-load("ParamInitUS_V738tab.rData") # ParamInit
+load("data/ParamInitUS_V738tab.rData") # ParamInit
 P  <- ParamInit[,1];
 names(P) <- rownames(ParamInit)
 ii <-  ParamInit[,5]==1
@@ -10,3 +10,4 @@ ParamInitZ <- ParamInit[ParamInit$Calib==1,]
 idZ0 <- ParamInitZ[,4]==0
 idZ1 <- ParamInitZ[,4]==1
 idZ2 <- ParamInitZ[,4]==2
+
