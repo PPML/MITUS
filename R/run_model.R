fefd33fba6aaacc10cc3e0e616b2b607a6209e09
nswartwo <nswartwo@gmail.com>
2018-03-28 15:00:36 -0400

    print results to .csv & sources in graphs.R

--- R/run_model.R
+++ R/run_model.R
@@ -1,19 +1,28 @@
 library(Rcpp)
+library(RcppArmadillo)
 
 source("R/param.R")
 source("R/add_params.R")
-sourceCpp("src/tb_model.cpp")
 
-m <- cSim( nYrs     =   2100-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+sourceCpp("src/tb_model.cpp")
+#system.time(
+
+m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
            rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist = dist            ,
-      vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
-      HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
-      mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
-      TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
-      LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
-      EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans )$Outputs
+           vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
+           HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
+           mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
+           TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
+           LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
+           EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans    ,
+           can_go   = can_go      , did_go=did_go           , dist_orig =dist_orig, dist_goal=dist_goal, dist_goal_v=dist_goal_v,
+           dist_orig_v=dist_orig_v )$Outputs
+#)
 
+results=data.frame(m)
+write.csv(results, file = paste("MITUS_results/results", Sys.time(),".csv"))
+source("R/graphs.R")
 
 v1<-cSim(      nYrs     =   2100-1950 ,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
@@ -36,8 +45,9 @@ v0<-cSim( nYrs     =   2100-1950,  nRes     = length(ResNam), rDxt      = rDxt  
       EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans )$V0
 
 m[,2]
+m[3,]
 v1
 plot(v1)
 v0
-
+plot(v0)
 
