a863e1a0f6c7707f28e1241e6692ae0ec185a8e7
nswartwo <nswartwo@gmail.com>
2018-04-02 11:10:52 -0400

    comments out lgtcurve func

--- R/add_params.R
+++ R/add_params.R
@@ -1,7 +1,7 @@
 #random variables from initial params that need to be sourced;
-LgtCurve <- function(StYr,Endyr,EndVal) { z <- log(1/0.005-1)
-zz  <- seq(-z*(1+2*(StYr-1950)/(Endyr-StYr)),z*(1+2*(2100-Endyr)/(Endyr-StYr)),by=(2*z)/(Endyr-StYr)/12)
-zz  <- as.numeric(EndVal)/(1+exp(-zz));  if(StYr>1950) { zz[1:((StYr-1950)*12)] <- 0 };    zz  }
+#LgtCurve <- function(StYr,Endyr,EndVal) { z <- log(1/0.005-1)
+#zz  <- seq(-z*(1+2*(StYr-1950)/(Endyr-StYr)),z*(1+2*(2100-Endyr)/(Endyr-StYr)),by=(2*z)/(Endyr-StYr)/12)
+#zz  <- as.numeric(EndVal)/(1+exp(-zz));  if(StYr>1950) { zz[1:((StYr-1950)*12)] <- 0 };    zz  }
 
 NixTrans <- rep(1,1801)
 #(c(1-LgtCurve(2016,2017,1)), rep(1,888))
