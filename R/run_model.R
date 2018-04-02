4382128f7310983658beeb369c3fd7633f99f083
nswartwo <nswartwo@gmail.com>
2018-04-02 11:15:41 -0400

    formatting

--- R/run_model.R
+++ R/run_model.R
@@ -1,13 +1,17 @@
 library(Rcpp)
 library(RcppArmadillo)
 
+#source("R/start_model.R")
 source("R/param.R")
 source("R/add_params.R")
+source("R/rebalancing population.R")
 
 sourceCpp("src/tb_model.cpp")
+source("R/graphs.R")
+
 #system.time(
 
-m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+m <- cSim( nYrs     =   2100-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
            rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist = dist            ,
            vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
@@ -16,13 +20,18 @@ m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt 
            TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
            LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
            EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans    ,
-           can_go   = can_go      , did_go=did_go           , dist_orig =dist_orig, dist_goal=dist_goal, dist_goal_v=dist_goal_v,
+           can_go   = can_go      , did_go=did_go           , dist_orig =dist_orig,  dist_goal=dist_goal, dist_goal_v=dist_goal_v,
            dist_orig_v=dist_orig_v )$Outputs
+colnames(m) <-ResNam
 #)
 
 results=data.frame(m)
+save(results,file = paste("data/results", Sys.time(),".rData"))
 write.csv(results, file = paste("MITUS_results/results", Sys.time(),".csv"))
-source("R/graphs.R")
+
+tb_graph_all(1993,2005,results)
+tb_graph_specific(1993,2005,results, "mort")
+
 
 v1<-cSim(      nYrs     =   2100-1950 ,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
