4f50164aa19d045a6b72fcde24d19bb4e1a48126
nswartwo <nswartwo@gmail.com>
2018-04-02 23:02:30 -0400

    updates cSim call for the rebalance inputs

--- R/run_model.R
+++ R/run_model.R
@@ -4,32 +4,35 @@ library(RcppArmadillo)
 #source("R/start_model.R")
 source("R/param.R")
 source("R/add_params.R")
-source("R/rebalancing population.R")
+source("R/param_init.R")
+source("R/gen_reblnc_pop.R")
 
 sourceCpp("src/tb_model.cpp")
 source("R/graphs.R")
 
 #system.time(
 
-m <- cSim( nYrs     =   2100-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
-           rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist = dist            ,
+           rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist_gen = dist_gen    ,
            vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
            HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
            mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
            TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
            LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
            EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans    ,
-           can_go   = can_go      , did_go=did_go           , dist_orig =dist_orig,  dist_goal=dist_goal, dist_goal_v=dist_goal_v,
-           dist_orig_v=dist_orig_v )$Outputs
+           can_go   = can_go      , did_go=did_go            , dist_goal=dist_goal, dist_goal_v=dist_goal_v  , diff_i_v = diff_i_v,
+           dist_orig_v=dist_orig_v, dist_new = dist_new )$Outputs
+
 colnames(m) <-ResNam
+
 #)
 
 results=data.frame(m)
 save(results,file = paste("data/results", Sys.time(),".rData"))
 write.csv(results, file = paste("MITUS_results/results", Sys.time(),".csv"))
 
-tb_graph_all(1993,2005,results)
+tb_graph_all(1950,2050,results)
 tb_graph_specific(1993,2005,results, "mort")
 
 
