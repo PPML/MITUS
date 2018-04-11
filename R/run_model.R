6957506a249c540d7369da6c86cb2d35d84e5e6b
nswartwo <nswartwo@gmail.com>
2018-04-11 16:39:32 -0400

    makes various changes to outputs for troubleshooting

--- R/run_model.R
+++ R/run_model.R
@@ -2,17 +2,19 @@ library(Rcpp)
 library(RcppArmadillo)
 
 #source("R/start_model.R")
-source("R/param.R")
-source("R/add_params.R")
-source("R/param_init.R")
-source("R/gen_reblnc_pop.R")
-
+# source("R/param.R")
+# source("R/add_params.R")
+ #source("R/param_init.R")
+source("R/process_sims.R")
+#source("R/gen_reblnc_pop.R")
 sourceCpp("src/tb_model.cpp")
 source("R/graphs.R")
 
+  OutputsZint(samp_i = 1, parAll200,endyr=2050,Int1=0,Int2=0,Int3=0,Int4=0,Int5=0,Scen1=0,Scen2=0,Scen3=0)
+  ##transmat
 #system.time(
 
-m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+g <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
            rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist_gen = dist_gen    ,
            vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
@@ -21,18 +23,36 @@ m <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt 
            TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
            LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
            EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans    ,
-           can_go   = can_go      , did_go=did_go            , dist_goal=dist_goal, dist_goal_v=dist_goal_v  , diff_i_v = diff_i_v,
-           dist_orig_v=dist_orig_v, dist_new = dist_new )$Outputs
+           can_go   = can_go      , dist_goal=dist_goal  , diff_i_v = diff_i_v,
+           dist_orig_v=dist_orig_v, dist_new = dist_new )$trans_mat
 
-colnames(m) <-ResNam
+g
+n <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+           Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
+           rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist_gen = dist_gen    ,
+           vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
+           HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
+           mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
+           TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
+           LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
+           EarlyTrend = EarlyTrend, EffLt    = EffLt         , EffLt0    = EffLt0   , dLtt     = dLtt        , NixTrans = NixTrans    ,
+           can_go   = can_go      , dist_goal=dist_goal  , diff_i_v = diff_i_v,
+           dist_orig_v=dist_orig_v, dist_new = dist_new )$dist_i_v
+n
 
-#)
+#
+# colnames(m) <-ResNam
+#
+#
+#
+# results<-data.frame(m)
 
-results=data.frame(m)
+
 save(results,file = paste("data/results", Sys.time(),".rData"))
 write.csv(results, file = paste("MITUS_results/results", Sys.time(),".csv"))
 
 tb_graph_all(1950,2050,results)
+tb_graph_vital(1950,2049, results)
 tb_graph_specific(1993,2005,results, "mort")
 
 
