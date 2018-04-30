f5086f67645381d51a4338dd0e87f5f7a199622a
nswartwo <nswartwo@gmail.com>
2018-04-30 16:01:14 -0400

    updates func inputs

---
+++ R/incid_prog_func.R
@@ -0,0 +1,16 @@
+#'Create a function of tb incidence to yield TB progression risk
+#'
+#'This script defines a function of TB incidence that will
+#'approximate the relationship between it and TB progression
+#'risk as defined in the model.
+#'
+#'@param ui_tbincid user defined tb incidence for target risk group
+#'@param targ_pop user defined size of target group to which the
+#'                ui_tbincid is true.
+#'@
+#'@return average rate of tb progression risk for target risk group
+#'@export
+
+incid_prog <- function(ui_tbincid){
+
+}
