3dd53f6049f2b37bce2b75ee20e137df4cdf8b70
nswartwo <nswartwo@gmail.com>
2018-05-16 16:29:42 -0400

    readds RRmuHR to func call

--- R/process_sims.R
+++ R/process_sims.R
@@ -15,7 +15,7 @@ source("R/gen_reblnc_pop.R")
 ################################################################################
 #########                      FUNCTION                                #########
 
-OutputsZint <-  function(samp_i=1,ParMatrix,endyr=2100,Int1=0,Int2=0,Int3=0,Int4=0,Int5=0,Scen1=0,Scen2=0,Scen3=0) {
+OutputsZint <-  function(samp_i=1,ParMatrix,endyr=2050,Int1=0,Int2=0,Int3=0,Int4=0,Int5=0,Scen1=0,Scen2=0,Scen3=0) {
    if(min(dim(as.data.frame(ParMatrix)))==1) { ;
     Par <- as.numeric(ParMatrix);
     names(Par) <- names(ParMatrix)
@@ -39,7 +39,7 @@ OutputsZint <-  function(samp_i=1,ParMatrix,endyr=2100,Int1=0,Int2=0,Int3=0,Int4
  m <-       cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
                            Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
                            rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist_gen = dist_gen    ,
-                           vTMort   = vTMort      , vRFMort = vRFMort        , RRmuHR    = RRmuHR   , muTbRF = muTbRF        , Birthst   = Birthst    ,
+                           vTMort   = vTMort      , RRmuRF = RRmuRF , RRmuHR = RRmuHR , muTbRF = muTbRF        , Birthst   = Birthst    ,
                            HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
                            mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
                            TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
