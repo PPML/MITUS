a292c62e1132440ebd30fb47237c0d36983b4f3c
nswartwo <nswartwo@gmail.com>
2018-06-06 19:50:42 -0400

    deleting unnecessary scripts

--- R/MITAS_environment.R
+++
@@ -1,8 +0,0 @@
-#' The MITAS Variable Environment
-#'
-#' @name MITAS_env
-#'
-#' @description This is the environment where variables and parameters necessary
-#' for the MITAS model will be stored.
-#'
-#'
