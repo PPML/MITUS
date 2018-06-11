94d8634b4701fa9d1d38846cd993f37d5816dd59
nswartwo <nswartwo@gmail.com>
2018-06-11 15:41:43 -0400

    initial commit

---
+++ R/IMIS_functions.R
@@ -0,0 +1,187 @@
+#'This script creates a function that loops over the log-likelihood
+#'functions found in calib_functions.R and updates the
+#'load the necessary libraries
+library(mnormt)
+library(parallel)
+library(lhs)
+
+#'@param samp_i
+#'@param ParMatrix ParInit
+#'@return lLik
+#'
+llikelihoodZ <-  function(samp_i,ParMatrix) {
+  Par <- ParMatrix[samp_i,]
+  # normal to uniform
+  Par2 <- pnorm(Par,0,1)
+  # uniform to true
+  Par3 <- Par2
+  Par3[idZ0] <- qbeta( Par2[idZ0], shape1  = ParamInitZ[idZ0,6], shape2 = ParamInitZ[idZ0,7])
+  Par3[idZ1] <- qgamma(Par2[idZ1], shape   = ParamInitZ[idZ1,6], rate   = ParamInitZ[idZ1,7])
+  Par3[idZ2] <- qnorm( Par2[idZ2], mean    = ParamInitZ[idZ2,6], sd     = ParamInitZ[idZ2,7])
+  P[ii] <- Par3
+  P <<- P
+
+  jj <- tryCatch({
+    source("R/param.R")
+    zz <- cSim( nYrs     =   2050-1950,  nRes     = length(ResNam), rDxt      = rDxt     , TxQualt   = TxQualt    , InitPop   = InitPop    ,
+                Mpfast   = Mpfast      , ExogInf   = ExogInf      , MpfastPI  = MpfastPI , Mrslow    = Mrslow     , rrSlowFB = rrSlowFB    ,
+                rfast    = rfast       , RRcurDef = RRcurDef      , rSlfCur  = rSlfCur   , p_HR     = p_HR        , dist_gen = dist_gen    ,
+                vTMort   = vTMort      , RRmuRF = RRmuRF          , RRmuHR=RRmuHR        , muTbRF = muTbRF        , Birthst   = Birthst    ,
+                HrEntEx   = HrEntEx    , ImmNon    = ImmNon       , ImmLat    = ImmLat   , ImmAct   = ImmAct      , ImmFst    = ImmFst     ,
+                mubt     = mubt        , RelInf   = RelInf        , RelInfRg  = RelInfRg , Vmix      = Vmix       , rEmmigFB  = rEmmigFB   ,
+                TxVec    = TxVec       , TunTxMort = TunTxMort    , rDeft     = rDeft    , pReTx     = pReTx      , LtTxPar  = LtTxPar     ,
+                LtDxPar  = LtDxPar     , rLtScrt   = rLtScrt      , RRdxAge  = RRdxAge   , rRecov   = rRecov      , pImmScen  = pImmScen   ,
+                EarlyTrend = EarlyTrend, NixTrans = NixTrans      , can_go   = can_go    , dist_goal=dist_goal    , diff_i_v  = diff_i_v   ,
+                dist_orig_v=dist_orig_v)
+#'if any output is missing or negative or if any model state population is negative
+#'set the likelihood to a hugely negative number (penalized)
+    if(sum(is.na(zz$Outputs[65,]))>0 | min(zz$Outputs[65,])<0 | min(zz$V1)<0 ) {
+      lLik <- -10^12
+  } else {
+      M <- zz$Outputs
+      colnames(M) <- ResNam
+      lLik <- 0
+      #' TOTAL DIAGNOSED CASES 1953-2014
+      v1   <- M[4:66,"NOTIF_ALL"]+M[4:66,"NOTIF_MORT_ALL"]
+      addlik <- notif_tot_lik(V=v1); addlik
+      lLik <- lLik + addlik
+      #' US CASES AGE DISTRIBUTION 1993-2013
+      v2a   <- M[44:65,235:245]+M[44:65,246:256]
+      addlik <- notif_age_us_lLik(V=v2a); addlik
+      lLik <- lLik + addlik
+      #' FB CASES AGE DISTRIBUTION 1993-2013
+      v2b   <- (M[44:65,137:147]+M[44:65,216:226]) - (M[44:65,235:245]+M[44:65,246:256])
+      addlik <- notif_age_fb_lLik(V=v2b); addlik
+      lLik <- lLik + addlik
+      #' CASES FB DISTRIBUTION 1993-2014
+      v3   <- cbind(M[44:66,154]+M[44:66,155]+(M[44:66,233]+M[44:66,234]),
+                    M[44:66,152]+M[44:66,153]+(M[44:66,231]+M[44:66,232]))
+      addlik <- notif_fb_lLik(V=v3); addlik
+      lLik <- lLik + addlik
+      #' CASES FB, US 2010-2014  SLOPE
+      v3   <- cbind(M[44:66,154]+M[44:66,155]+(M[44:66,233]+M[44:66,234]),
+                    M[44:66,152]+M[44:66,153]+(M[44:66,231]+M[44:66,232]))
+      addlik <- notif_fbus_slp_lLik(V=v3); addlik
+      lLik <- lLik + addlik
+      #' CASES HR DISTRIBUTION 1993-2014
+      v5b   <- cbind(M[44:65,153],M[44:65,152]) + cbind(M[44:65,232],M[44:65,231])
+      addlik <- notif_us_hr_lLik(V=v5b); addlik
+      lLik <- lLik + addlik
+      #' CASES FB RECENT ENTRY DISTRIBUTION 1993-2014
+      v6   <- M[44:65,154:155]+M[44:65,233:234]
+      addlik <- notif_fb_rec_lLik(V=v6); addlik
+      lLik <- lLik + addlik
+      #' TREATMENT OUTCOMES 1993-2012
+      v11  <- M[44:63,133:135]
+      addlik <- tx_outcomes_lLik(V=v11); addlik
+      lLik <- lLik + addlik
+      #' TOTAL LTBI TREATMENT INITS 2002
+      v12  <- M[53,176]
+      addlik <- tltbi_tot_lLik(V=v12); addlik
+      lLik <- lLik + addlik
+      #' DIST LTBI TREATMENT INITS 2002
+      v13  <- M[53,177:179]/M[53,176]
+      addlik <- tltbi_dist_lLik(V=v13)*2; addlik
+      lLik <- lLik + addlik
+      #' LTBI PREVALENCE BY AGE 2011, US
+      v15  <- cbind(M[62,56:66],M[62,34:44]-M[62,56:66])
+      v15a <- outer(v15[,1],c(SensLt,1-SensLt))+outer(v15[,2],c(1-SpecLt,SpecLt))
+      addlik <- ltbi_us_11_lLik(V=v15a)*2; addlik
+      lLik <- lLik + addlik
+      #' LTBI PREVALENCE BY AGE 2011, FB
+      v16  <- cbind(M[62,67:77],M[62,45:55]-M[62,67:77])
+      v16a <- outer(v16[,1],c(SensLt,1-SensLt))+outer(v16[,2],c(1-SpecLt,SpecLt))
+      addlik <- ltbi_fb_11_lLik(V=v16a)*2; addlik
+      lLik <- lLik + addlik
+      #' TOTAL POP EACH DECADE, BY US/FB
+      v17  <- M[,32]+M[,33]
+      addlik <- tot_pop_yr_fb_lLik(V=v17); addlik
+      lLik <- lLik + addlik
+      #' TOTAL POP AGE DISTRIBUTION 2014
+      v18  <- cbind(M[65,34:44],M[65,45:55])
+      addlik <- tot_pop14_ag_fb_lLik(V=v18); addlik
+      lLik <- lLik + addlik
+      #' TOTAL DEATHS WITH TB 1999-2014
+      v19  <- M[50:65,279:289]   ### THIS NOW ALL TB DEATHS
+      addlik <- tb_dth_tot_lLik(V=v19); addlik
+      lLik <- lLik + addlik
+      #' TB DEATHS 1999-2014 BY AGE
+      addlik <- tb_dth_age_lLik(V=v19); addlik
+      lLik <- lLik + addlik
+      #' TOTAL DEATHS WITH HIV 1999-2014
+      v20  <- M[61:64,111:121]
+      addlik <- hv_dth_tot_lLik(V=v20); addlik
+      lLik <- lLik + addlik
+      #' HOMELESS POP 2010
+      v23b  <- M[61,31]
+      addlik <- homeless_10_lLik(V=v23b); addlik
+      lLik <- lLik + addlik
+      #' LIKELIHOOD FOR BORGDORFF, FEREBEE & SUTHERLAND ESTIMATES
+      v2456  <- c(pfast,pimmed,rslow,rfast,rRecov)
+      addlik <- borgdorff_lLik( Par=v2456); addlik
+      lLik <- lLik + addlik
+      addlik <- ferebee_lLik(   Par=v2456); addlik
+      lLik <- lLik + addlik
+      addlik <- sutherland_lLik(Par=v2456); addlik
+      lLik <- lLik + addlik
+
+
+      ### ### ### FB RT LIKELIHOOD
+      #   v30  <-  (M[66,212]/M[66,195])
+      #   addlik <- dnorm(v30,0.075,0.0125,log=T)-dnorm(0.075,0.075,0.0125,log=T); addlik
+      #   lLik <- lLik + addlik
+      ### ### ###  ALL LIKELIHOODS DONE !!
+
+    } }, error = function(e) NA)
+  if(is.na(jj))         { lLik <- -10^12 - sum((ParamInitZ[,8]-Par)^2) }
+  if(jj%in%c(-Inf,Inf)) { lLik <- -10^12 - sum((ParamInitZ[,8]-Par)^2) }
+
+  return((lLik))  }
+
+#'Local parallelization via multicore
+#'@param ParMatrix
+#'@n_cores
+#'@lLik
+llikelihood <- function(ParMatrix,n_cores=1) {
+  if(dim(as.data.frame(ParMatrix))[2]==1) {
+    lLik <- llikelihoodZ(1,t(as.data.frame(ParMatrix)))
+  } else {
+    lLik <- unlist(mclapply(1:nrow(ParMatrix),llikelihoodZ,ParMatrix=ParMatrix,mc.cores=n_cores))
+  }
+return((lLik))
+}
+
+#'lprior function
+#'@param ParMatrix
+#'@return lPri
+
+lprior <- function(ParMatrix) { # Par = ParInit
+  if(dim(as.data.frame(ParMatrix))[2]==1) {
+    ParMatrix <- t(as.data.frame(ParMatrix)) }
+  lPri <- rep(0,nrow(ParMatrix))
+  for(samp_i in 1:nrow(ParMatrix)) {
+    # norm2unif
+    Par <- ParMatrix[samp_i,]
+    Par2 <- pnorm(Par,0,1)
+    # unif2true
+    Par3 <- Par2
+    Par3[idZ0] <- qbeta( Par2[idZ0], shape1  = ParamInitZ[idZ0,6], shape2 = ParamInitZ[idZ0,7])
+    Par3[idZ1] <- qgamma(Par2[idZ1], shape   = ParamInitZ[idZ1,6], rate   = ParamInitZ[idZ1,7])
+    Par3[idZ2] <- qnorm( Par2[idZ2], mean    = ParamInitZ[idZ2,6], sd     = ParamInitZ[idZ2,7])
+    lPri[samp_i] <- lPrior2(Par,Par3)
+    if(is.na(lPri[samp_i]))         {
+      lPri[samp_i] <- -10^12 - sum((ParamInitZ[,8]-Par)^2) }
+    if(lPri[samp_i]%in%c(-Inf,Inf)) {
+      lPri[samp_i] <- -10^12 - sum((ParamInitZ[,8]-Par)^2) }
+  } # end inner loop
+  return(lPri)
+}
+
+#'sample the prior function
+#'@param n
+#'@return
+sample.prior1 <- function(n) {
+  rmnorm(n,rep(0,sum(ParamInit$Calib==1)),diag(sum(ParamInit$Calib==1))) }
+sample.prior2 <- function(n) {
+  qnorm(randomLHS(n,sum(ParamInit$Calib==1)),0,1)   }
+sample.prior  <- sample.prior2
