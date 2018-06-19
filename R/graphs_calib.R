81539b2aea2cb90c45b23f82e99713aed671d00f
nswartwo <nswartwo@gmail.com>
2018-06-19 15:30:09 -0400

    adds calc for reactiv rate and fraction w/o TB

--- R/graphs_calib.R
+++ R/graphs_calib.R
@@ -3,18 +3,12 @@
 #'
 #'load the necessary library
 library(MCMCpack)
+
+calib_graphs <- function(dataset){
 #'load the parameter data set & model inputs
 load("data/ParamInitUS_V738tab.rData") # ParamInit
 load("data/ModelInputs_9-2-16.rData")
 
-#'source in other scripts and functions
-source("R/param.R")
-source("PriorFuncUS_V22.r")
-source("R/calib_functions.R")
-source("src/tb_model.cpp")
-source("R/IMIS_functions.R")
-source("R/process_sims.R")
-#'
 #'Define the Posterior Function
 #'@param theta
 #'@return posterior
@@ -23,13 +17,14 @@ posterior <- function(theta){
 }
 #' load the optimized data set
 load("data/Opt_US5401_r8_1_1-27-16.rData")
-o10
-par_1 <- o10$par
+dataset #this is the name of the data set above
+
+par_1 <- dataset$par
 
 llikelihood(par_1)
 lprior(par_1)
 posterior(par_1)
-M1    <- Outputs(par_1)
+M1    <- OutputsZint(ParMatrix=par_1)
 M <- M1
 #save(M,file="M_6-2-16c.rData")
 lPrior2
@@ -41,7 +36,7 @@ Par3[idZ0] <- qbeta( Par2[idZ0], shape1  = ParamInitZ[idZ0,6], shape2 = ParamIni
 Par3[idZ1] <- qgamma(Par2[idZ1], shape   = ParamInitZ[idZ1,6], rate   = ParamInitZ[idZ1,7])
 Par3[idZ2] <- qnorm( Par2[idZ2], mean    = ParamInitZ[idZ2,6], sd     = ParamInitZ[idZ2,7])
 P[ii] <- Par3
-P <<- P
+P <- P
 reldens <- Par3
 reldens[idZ0] <- dbeta( Par3[idZ0], shape1  = ParamInitZ[idZ0,6], shape2 = ParamInitZ[idZ0,7],log=T)-dbeta( ParamInit$BcValue[ii][idZ0], shape1  = ParamInitZ[idZ0,6], shape2 = ParamInitZ[idZ0,7],log=T)
 reldens[idZ1] <- dgamma(Par3[idZ1], shape   = ParamInitZ[idZ1,6], rate   = ParamInitZ[idZ1,7],log=T)-dgamma(ParamInit$BcValue[ii][idZ1], shape   = ParamInitZ[idZ1,6], rate   = ParamInitZ[idZ1,7],log=T)
@@ -49,22 +44,23 @@ reldens[idZ2] <- dnorm( Par3[idZ2], mean    = ParamInitZ[idZ2,6], sd     = Param
 reldens2 <- P
 reldens2[] <- 0
 reldens2[ii] <- round(reldens,2)
-csv
-write.csv(cbind(ParamInit,P,reldens2),file=paste("Parameter_table_", Sys.time,".csv", sep=''))
+
+fn = paste("Parameter_table_", Sys.time(),".csv", sep='')
+write.csv(cbind(ParamInit,P,reldens2),file=fn)
 
 source("R/param.R")
 
 #'Name and Open a .pdf file to which the plots will be written.
-pdfnam <-paste("Fig_Calib_Checks_",Sys.time(), ".pdf", sep = "")
+pdfnam <-paste("Fig_Calib_Checks_",Sys.time(),".pdf", sep = "")
 pdf(file=pdfnam,width=11, height=8.5)
 par(mfrow=c(2,2),mar=c(4,4.5,3,1))
 
 #' graph of total diagnosed cases
 #' by total population, US born population, and non-US born population
 #' 1953-2013
-V0 <- df[4:66,"NOTIF_ALL"]+df[4:66,"NOTIF_MORT_ALL"] #total population
-V1 <- df[44:66,"NOTIF_US"]+df[44:66,"NOTIF_MORT_US"]   #US born population
-V2 <- df[44:66,"NOTIF_F1"]+df[44:66,"NOTIF_F2"]+df[44:66,"NOTIF_MORT_F1"]+df[44:66,"NOTIF_MORT_F2"]   #non-US born population
+V0 <- M[4:66,"NOTIF_ALL"]+M[4:66,"NOTIF_MORT_ALL"]   #total population
+V1 <- M[44:66,"NOTIF_US"]+M[44:66,"NOTIF_MORT_US"]   #US born population
+V2 <- M[44:66,"NOTIF_F1"]+M[44:66,"NOTIF_F2"]+M[44:66,"NOTIF_MORT_F1"]+M[44:66,"NOTIF_MORT_F2"]   #non-US born population
 
 #'format the plot
 plot(0,0,ylim=c(0,max(range(V0*1e3))),xlim=c(1954,2015),xlab="",ylab="",axes=F)
@@ -123,9 +119,9 @@ legend("topright",c("Reported data (all)","Reported data (US born)","Reported da
 
 #'Percent of Total Cases Non-US Born Population
 
-V <- cbind(df[44:66,"NOTIF_US"]+df[44:66,"NOTIF_MORT_US"], #US born population
-           df[44:66,"NOTIF_F1"]+df[44:66,"NOTIF_F2"]+  #non-US born population
-             df[44:66,"NOTIF_MORT_F1"]+df[44:66,"NOTIF_MORT_F2"])
+V <- cbind(M[44:66,"NOTIF_US"]+M[44:66,"NOTIF_MORT_US"], #US born population
+           M[44:66,"NOTIF_F1"]+M[44:66,"NOTIF_F2"]+  #non-US born population
+             M[44:66,"NOTIF_MORT_F1"]+M[44:66,"NOTIF_MORT_F2"])
 V <- V[,2]/rowSums(V)
 
 #'format the plot
@@ -148,7 +144,7 @@ legend("topleft",c("Reported data","Model"),pch=c(19,NA),lwd=c(1,2),col=c(1,4),l
 ################################################################################
 #'Percent of Non-US Born Cases from Recent Immigrant Population
 
-V <- cbind(df[44:65,"NOTIF_F1"]+df[44:65,"NOTIF_MORT_F1"],df[44:65,"NOTIF_F2"]+df[44:65,"NOTIF_MORT_F2"])
+V <- cbind(M[44:65,"NOTIF_F1"]+M[44:65,"NOTIF_MORT_F1"],M[44:65,"NOTIF_F2"]+M[44:65,"NOTIF_MORT_F2"])
 V <- V[,1]/rowSums(V)
 
 #'format the plot
@@ -172,7 +168,7 @@ legend("topright",c("Reported data","Model"),pch=c(19,NA),lwd=c(1,2),col=c(1,4),
 #'Age distribution of Cases
 #'0-24 yrs, 25-44 yrs, 45-64 yrs, 65+ yrs
 
-V   <- (df[51:65,136:146]+df[51:65,189:199])
+V   <- (M[51:65,136:146]+M[51:65,189:199])
 V2  <- V[,-11]
 V2[,10] <- V2[,10]+V[,11]
 
@@ -212,7 +208,7 @@ legend("topright",c("0-24 years","25-44 years","45-64 years","65+ years","Report
 #'Age Distribution of TB Cases in Percentages
 #'0-24 yrs, 25-44 yrs, 45-64 yrs, 65+ yrs
 #'
-V   <- (df[51:65,136:146]+df[51:65,189:199])
+V   <- (M[51:65,136:146]+M[51:65,189:199])
 V2  <- V[,-11]
 V2[,10] <- V2[,10]+V[,11]
 V2  <- colSums(V2)/sum(V2)*100
@@ -265,7 +261,7 @@ legend("topright",c("Reported data","Fitted model"),pch=c(19,NA),lwd=c(1,2),col=
 ################################################################################
 #' Treatment Outcomes 1993-2011
 
-V   <- df[44:63,132:134]
+V   <- M[44:63,132:134]
 Vdisc <- V[,2]/rowSums(V)
 Vdead <- V[,3]/rowSums(V)
 
@@ -316,7 +312,7 @@ legend("bottomleft",c("Foreign-born","Homeless","Reported data","Fitted model"),
 
 #'LTBI Prevalance by Age in 2011, US born
 
-V  <- cbind(df[62,55:65],(df[62,33:43]-df[62,55:65]))
+V  <- cbind(M[62,55:65],(M[62,33:43]-M[62,55:65]))
 
 V1 <- V[,-22]; V1<-V1[,-21]
 V1[,20] <- V1[,20]+V[,21]+V[,22]
@@ -351,7 +347,7 @@ legend("topleft",c("Reported data","Model"),pch=c(19,15),lwd=c(0,NA),
 ################################################################################
 #'LTBI Prevalance by Age in 2011, non-US born
 
-V  <- cbind(df[62,66:76],(df[62,44:54]-df[62,66:76]))
+V  <- cbind(M[62,66:76],(M[62,44:54]-M[62,66:76]))
 
 V1 <- V[,-22]; V1<-V1[,-21]
 V1[,20] <- V1[,20]+V[,21]+V[,22]
@@ -386,7 +382,7 @@ legend("topleft",c("Reported data","Model"),pch=c(19,15),lwd=c(0,NA),
 ################################################################################
 #' Age Distribution of TB Deaths 1999-2013
 
-V  <- df[50:65,88:98]+df[50:65,99:109]
+V  <- M[50:65,88:98]+M[50:65,99:109]
 V2 <- V[,-11]; V2[,10] <- V[,10]+V[,11]
 V3 <- colSums(V2)*1e6
 
@@ -415,7 +411,7 @@ legend("topleft",c("Reported data","Model"),pch=c(19,15),lwd=NA,
 #'Total Population Each Decade
 #'US and non-US born
 
-V  <- cbind(df[1:66,30], df[1:66,31]+df[1:66,32])
+V  <- cbind(M[1:66,30], M[1:66,31]+M[1:66,32])
 
 #' format the plot
 plot(1,1,ylim=c(2,500),xlim=c(1950,2015),xlab="",ylab="",axes=F,log="y")
@@ -444,7 +440,7 @@ legend("bottomright",c("Total","US born","Foreign born","Reported data","model")
 
 #'Total Population Age Distribution 2014
 
-V  <- cbind(t(df[65,33:43]), t(df[65,44:54]))
+V  <- cbind(t(M[65,33:43]), t(M[65,44:54]))
 V1  <- V[-3,]
 V1[2,] <- V1[2,]+V[3,]
 V2 <- V1[-4,]
@@ -474,7 +470,22 @@ mtext("Population by Age for FB (red) and US (blue), 2014 (mil)",3,.8,font=2,cex
 legend("topright",c("Reported data","model"),pch=c(19,15),pt.cex=c(1,2),
        lwd=NA,col=c("grey30","grey80"),bg="white")
 
+################################################################################
+#' Recent TB infection 2014 (indices have been updated. )
+Vall <- (M[65,156:171]/M[65,172:187])
+#'format the plot
+plot(-1,0,ylim=c(0.02,0.8),xlim=c(0.5,17.5),xlab="",ylab="",axes=F)
+axis(2,las=2);box()
+axis(1,1:16,c("All",paste(c("0-4","5-14","15-24","25-34","35-44","45-54","55-64","65-74","75-84","85-94","95+"),
+            "yrs"),"US born","Foreign born","FB >2yrs","Homeless"),tick=F,cex.axis=0.7,las=2, mgp=c(3, 0.25, 0))
+abline(h=axTicks(2),col="grey85")
 
+#'plot the model data
+for(i in 1:16) lines(rep(i,2),c(0,Vall[i]),col="forestgreen",lwd=10,lend="butt")
+
+#'plot text
+text(1:16,Vall,format(round(Vall,2),nsmall=2),cex=0.7,pos=3)
+mtext("Fraction of Incident TB from Recent Infection (<2 years)",3,.8,font=2,cex=0.8)
 ################################################################################
 ################################################################################
 ### ### ### VARIOUS DEATHS DATA ### ### ### ### ### ###
@@ -496,111 +507,120 @@ legend("topright",c("Reported data","model"),pch=c(19,15),pt.cex=c(1,2),
 # legend("topright",c("Dead at TB diagnosis or while on TB treatment","Died while on TB treatment","ICD-10 MCD TB codes"),
 #        col=c(4,3,2),lwd=3,bg="white")
 
-
 ################################################################################
-#' Other TB things!
+#' Other TB things! comparisons to published estimates
 #'Reactivation Rates vs. Ferebee and Sutherland estimates
+#'source in param_init.R or param.R (will need to finalize)
+# source("R/param_init.R")
 n_yr_Z = 50
-p2 <-  pfast*(1-(1-exp(-(rfast+rRecov)*12*(0:n_yr_Z)))*(rfast/(rfast+rRecov))) +
-      (1-pfast)*(1-(1-exp(-(rslow+rRecov)*12*(0:n_yr_Z)))*(rslow/(rslow+rRecov)))
-r2 <- -log(1-diff(-p2)/p2[-(n_yr_Z+1)])/1
+pfast_v <- rslow_v <- rep(NA,4)
+pfast_v <- pfast * exp(0:3)/3*log(ORpfastRF)
+rslow_v <- rslow * exp(0:3)/3*log(RRrslowRF)
+s2 <- rep(NA,4)
+#' calculate the joint probability of pfast/pslow & not progressing to active disease & not recovering
+for (i in 1:4){
+  p2 <- pfast_v[i] *(1-(1-exp(-(rfast+rRecov)*(0:n_yr_F)))*(rfast/(rfast+rRecov))) +
+    (1-pfast_v[i])*(1-(1-exp(-(rslow_v[i]+rRecov)*(0:n_yr_F)))*(rslow_v[i]/(rslow_v[i]+rRecov)))
+  r2 <- -log(1-diff(-p2)/p2[-(n_yr_F+1)])/1
+  s2[i]<- sum((datF[,3]*log(r2) + (datF[,2]-datF[,3])*log(1-r2))/N_red)-adj_25/N_red
+}
+V<-(sum(s2*colSums(dist_gen)))
 
-#' format the plot
-plot(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,type="l",lwd=2,xlim=c(0,15),ylim=c(0,60),las=1,xlab="",ylab="")
+#' #' format the plot
+#' plot(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,type="l",lwd=2,xlim=c(0,15),ylim=c(0,60),las=1,xlab="",ylab="")
+#'
+#' #' plot the model data
+#' lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2, col=)
+#' lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2, col=)
+#'
+#' #' reported data for comparison
+#' lines(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,lwd=2)
+#' lines(1:nrow(datS)-.5,datS[,3]/datS[,2]*1000,lwd=2,lty=2)
+#'
+#' #' plot text
+#' mtext("Years since infection",1,2.5,cex=0.9)
+#' mtext("Reactivation rate per 1000 PY",2,3,cex=0.8)
+#' legend("topright",c("Ferebee 1970","Sutherland 1968","Fitted model"),
+#'        col=c(1,1,4),lty=c(1,2,1),lwd=2,cex=0.9)
+#' mtext("Reactivation rate vs. Ferebee, Sutherland estimates",3,0.8,cex=0.8,font=2)
+#' ################################################################################
+#' #'Reactivation Rates vs. Ferebee and Sutherland estimates on Log Scale
+#'
+#' #'format the plot
+#' plot(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,type="l",lwd=2,xlim=c(0,20),ylim=c(0.2,55),las=1,xlab="",ylab="",log="y")
+#'
+#' #' plot the model data
+#' lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2, col=)
+#' lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2, col=)
+#'
+#' #' reported data for comparison
+#' lines(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,lwd=2)
+#' lines(1:nrow(datS)-.5,datS[,3]/datS[,2]*1000,lwd=2,lty=2)
+#'
+#' #' plot text
+#' mtext("Years since infection",1,2.5,cex=0.9)
+#' mtext("Reactivation rate per 1000 PY (log scale)",2,3,cex=0.8)
+#' legend("topright",c("Ferebee 1970","Sutherland 1968","Fitted model"),
+#'        col=c(1,1,4),lty=c(1,2,1),lwd=2,cex=0.9)
+#' mtext("Reactivation rate vs Ferebee & Sutherland estimates, log-scale",3,0.8,cex=0.8,font=2)
+#' ################################################################################
+#' #'Fraction without active TB (of those progressing <15yrs)
+pfast_v <- rslow_v <- rep(NA,4)
+pfast_v <- pfast * exp(0:3)/3*log(ORpfastRF)
+rslow_v <- rslow * exp(0:3)/3*log(RRrslowRF)
 
-#' plot the model data
-lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2)
-
-#' reported data for comparison
-lines(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,lwd=2)
-lines(1:nrow(datS)-.5,datS[,3]/datS[,2]*1000,lwd=2,lty=2)
-
-#' plot text
-mtext("Years since infection",1,2.5,cex=0.9)
-mtext("Reactivation rate per 1000 PY",2,3,cex=0.8)
-legend("topright",c("Ferebee 1970","Sutherland 1968","Fitted model"),
-       col=c(1,1,4),lty=c(1,2,1),lwd=2,cex=0.9)
-mtext("Reactivation rate vs. Ferebee, Sutherland estimates",3,0.8,cex=0.8,font=2)
-################################################################################
-#'Reactivation Rates vs. Ferebee and Sutherland estimates on Log Scale
-
-#'format the plot
-plot(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,type="l",lwd=2,xlim=c(0,20),ylim=c(0.2,55),las=1,xlab="",ylab="",log="y")
-
-#' plot the model data
-lines(1:n_yr_Z-.5,r2*1000,col=4,lwd=2)
-
-#' reported data for comparison
-lines(1:nrow(datF)-.5,datF[,3]/datF[,2]*1000,lwd=2)
-lines(1:nrow(datS)-.5,datS[,3]/datS[,2]*1000,lwd=2,lty=2)
-
-#' plot text
-mtext("Years since infection",1,2.5,cex=0.9)
-mtext("Reactivation rate per 1000 PY (log scale)",2,3,cex=0.8)
-legend("topright",c("Ferebee 1970","Sutherland 1968","Fitted model"),
-       col=c(1,1,4),lty=c(1,2,1),lwd=2,cex=0.9)
-mtext("Reactivation rate vs Ferebee & Sutherland estimates, log-scale",3,0.8,cex=0.8,font=2)
-################################################################################
-#'Fraction without active TB (of those progressing <15yrs)
-
-p <- pfast*pimmed*c(1,rep(0,nrow(datB)-1)) +
-  pfast*(1-pimmed)*(1-(1-exp(-(rfast+rRecov)*12*datB[,1]))*(rfast/(rfast+rRecov))) +
-  (1-pfast)*(1-(1-exp(-(rslow+rRecov)*12*datB[,1]))*(rslow/(rslow+rRecov)))
-p <- 1-(1-p)/(1-p)[nrow(datB)]
-
-#'format the plot
-plot(datB[,1:2],type="l",lwd=1,lty=3,las=1,xlab="",ylab="")
-
-#' plot the model data
-points(datB[,1:2],cex=0.8,pch=19)
-lines(datB[,1],p,col=4,lwd=2)
-
-#' plot text
-mtext("Years since infection",1,2.5,cex=0.9)
-mtext("Fraction without active TB",2,3,cex=0.8)
-legend("topright",c("Borgdorff 2011","Fitted model"), col=c(1,4),lwd=3,cex=0.9)
-mtext("Fraction without active TB (of those progressing <15yrs)",3,0.8,cex=0.8,font=2)
-
-################################################################################
-#'Cumulative incidence of active TB following M.tb infection at age 15 (%)
-vec <- rep(0,4); names(vec) <- c("safe","slow","fast","prog")
-survM15 <- NULL
-for(i in 1:(50*12)) {
-  if(i==1) {  vec <- c(0,1-Mpfast[3,1],Mpfast[3,1]*(1-pimmed),Mpfast[3,1]*pimmed) } else {
-    vec[1] <- vec[1]+vec[2]*rRecov
-    vec[4] <- vec[4]+vec[3]*rfast+vec[2]*rep(Mrslow[3:11,1],each=10*12)[i]
-    vec[2] <- vec[2]-vec[2]*rRecov-vec[2]*rep(Mrslow[3:11,1],each=10*12)[i]
-    vec[3] <- vec[3]-vec[3]*rfast  }
-  survM15[i] <- 1- vec[4]  }
-
-#'format the plot
-plot(-1,-1,col=0,las=1,ylim=c(0,10),xlim=c(0,50),xlab="",ylab="")
-abline(h=axTicks(2),col="grey90");box()
-#'plot the model data
-lines(seq(0,50*12)/12,c(0,(1-survM15)*100),col=4,lwd=2)
-#' plot text
-mtext("Years since infection",1,2.5,cex=0.9)
-mtext("Cumulative incidence of active TB following M.tb infection at age 15 (%)",3,0.8,cex=0.8,font=2)
-
-################################################################################
-#' Recent TB infection 2014 (indices have been updated. )
-Vall <- (M[65,156:171]/M[65,172:187])
-#'format the plot
-plot(-1,0,ylim=c(0.02,0.8),xlim=c(0.5,17.5),xlab="",ylab="",axes=F)
-axis(2,las=2);box()
-axis(1,1:16,c("All",paste(c("0-4","5-14","15-24","25-34","35-44","45-54","55-64","65-74","75-84","85-94","95+"),
-            "yrs"),"US born","Foreign born","FB >2yrs","Homeless"),tick=F,cex.axis=0.7,las=2, mgp=c(3, 0.25, 0))
-abline(h=axTicks(2),col="grey85")
-
-#'plot the model data
-for(i in 1:16) lines(rep(i,2),c(0,Vall[i]),col="forestgreen",lwd=10,lend="butt")
-
-#'plot text
-text(1:16,Vall,format(round(Vall,2),nsmall=2),cex=0.7,pos=3)
-mtext("Fraction of Incident TB from Recent Infection (<2 years)",3,.8,font=2,cex=0.8)
+for (i in 1:4){
+  p <- pfast_v[i] *(1-(1-exp(-(rfast+rRecov)*datB[,1]))*(rfast/(rfast+rRecov))) +
+    (1-pfast_v[i])*(1-(1-exp(-(rslow_v[i]+rRecov)*datB[,1]))*(rslow_v[i]/(rslow_v[i]+rRecov)))
+  p <- 1-(1-p)/(1-p)[nrow(datB)]
+  s[i]<-sum(diff(-datB[,2])*ss_borgdorff*log(diff(-p)) + (1-diff(-datB[,2]))*ss_borgdorff*log(1-diff(-p)))/N_red - adj_24/N_red
+}
+s*(colSums(dist_gen)) #weighted average of the likelihoods
+#'
+#' #'format the plot
+#' plot(datB[,1:2],type="l",lwd=1,lty=3,las=1,xlab="",ylab="")
+#'
+#' #' plot the model data
+#' points(datB[,1:2],cex=0.8,pch=19, col=)
+#' lines(datB[,1],p,col=4,lwd=2, col=)
+#' points(datB[,1:2],cex=0.8,pch=19, col=)
+#' lines(datB[,1],p,col=4,lwd=2, col=)
+#'
+#'
+#' #' plot text
+#' mtext("Years since infection",1,2.5,cex=0.9)
+#' mtext("Fraction without active TB",2,3,cex=0.8)
+#' legend("topright",c("Borgdorff 2011","Fitted model"), col=c(1,4),lwd=3,cex=0.9)
+#' mtext("Fraction without active TB (of those progressing <15yrs)",3,0.8,cex=0.8,font=2)
+#'
+#' ################################################################################
+#' #'Cumulative incidence of active TB following M.tb infection at age 15 (%)
+#' #'needs to be updated!!!
+#' vec <- rep(0,3);
+#' names(vec) <- c("safe","slow","fast")
+#' survM15 <- NULL
+#' for(i in 1:(50*12)) {
+#'   if(i==1) {
+#'     vec <- c(0,1-P[["pfast"]][3,1],P[["pfast"]][3,1]) #need to update these values
+#'   } else {
+#'     vec[1] <- vec[1]+vec[2]*P[["rRecov"]]
+#'     vec[4] <- vec[4]+vec[3]*P[["rfast"]]+vec[2]*rep(P[["rslow"]][3:11,1],each=10*12)[i]
+#'     vec[2] <- vec[2]-vec[2]*P[["rRecov"]]-vec[2]*rep(P[["rslow"]][3:11,1],each=10*12)[i]
+#'     vec[3] <- vec[3]-vec[3]*P[["rfast"]]  }
+#'   survM15[i] <- 1- vec[4]  }
+#'
+#' #'format the plot
+#' plot(-1,-1,col=0,las=1,ylim=c(0,10),xlim=c(0,50),xlab="",ylab="")
+#' abline(h=axTicks(2),col="grey90");box()
+#' #'plot the model data
+#' lines(seq(0,50*12)/12,c(0,(1-survM15)*100),col=4,lwd=2)
+#' #' plot text
+#' mtext("Years since infection",1,2.5,cex=0.9)
+#' mtext("Cumulative incidence of active TB following M.tb infection at age 15 (%)",3,0.8,cex=0.8,font=2)
 
 ################################################################################
 
 
 dev.off(); system(paste("open", pdfnam)) # code for ma
 
+}
