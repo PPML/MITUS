137c1b5dfb5ddf2ebdf73959f4232c7efb1935e6
nswartwo <nswartwo@gmail.com>
2018-06-18 20:08:50 -0400

    removes unnecessary script calls

--- R/param_init.R
+++ R/param_init.R
@@ -20,7 +20,6 @@
 
 param_init <- function(ParVector,Int1=0,Int2=0,Int3=0,Int4=0,Int5=0,Scen1=0,Scen2=0,Scen3=0){
 load("data/ModelInputs_9-2-16.rData")
-# source("R/gen_reblnc_pop.R")
 #'Create an empty list to hold the formatted intitial parameters
 InputParams <-vector("list", 45)
 names(InputParams) <- c("rDxt","TxQualt", "InitPop", "Mpfast", "ExogInf", "MpfastPI",
