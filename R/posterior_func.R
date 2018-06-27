2be95f2a93d3e230563fbefe9206fff275d4ec66
nswartwo <nswartwo@gmail.com>
2018-06-27 15:18:00 -0400

    updates documentation

---
+++ R/posterior_func.R
@@ -0,0 +1,10 @@
+#'This function calculates the posterior using the
+#'lprior and llikelihood functions.
+#'@name posterior
+#'@param theta vector of values
+#'@param n_cores number of cores to use for the optimization
+#'@return posterior
+#'@export
+posterior <- function(theta) {
+  -lprior(theta) - llikelihood(theta,n_cores)
+}
