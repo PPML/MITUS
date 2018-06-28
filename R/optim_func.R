357dcd839113ca64e90bbf2236f3bd9346927b9d
nswartwo <nswartwo@gmail.com>
2018-06-28 15:31:34 -0400

    fixes typos in data load

--- R/optim_func.R
+++ R/optim_func.R
@@ -13,7 +13,7 @@
 
 optim_b <- function(b){
 
-  load("/data/ParamInit_2018.RData")
+  load("/data/ParamInit_2018.rData")
   P  <- ParamInit[,1]
 names(P) <- rownames(ParamInit)
 ii <-  ParamInit[,5]==1
