61274330c543b00d24f61a7737ad673b90cdda18
nswartwo <nswartwo@gmail.com>
2018-06-28 15:09:30 -0400

    creates func to sample prior & make StartVals rData file

---
+++ R/startval_func.R
@@ -0,0 +1,16 @@
+#'Function to create a Starting Values for the Parameters
+#'to be Optimized.
+#'
+#'@name gen_st_val
+#'@param n number of datasets you want to generate
+#'@param samp Which sampling method to use. "NORM" for random normal samples or "LHS" for latin hypercube sampling.
+#'@return rData file of starting values
+#'@export
+
+gen_st_val <-function(n=10, samp="LHS"){
+  if(identical(samp,"LHS")==FALSE & identical(samp,"NORM")==FALSE) stop("'samp' must be either 'LHS' or 'NORM'")
+  if(identical(samp,"NORM")==TRUE) StartVals <-sample.prior1(n)
+  if(identical(samp,"LHS") ==TRUE)StartVals <- sample.prior(n)
+  save(StartVals, file=paste("StartVal_", Sys.Date(),".rData", sep=""))
+}
+
