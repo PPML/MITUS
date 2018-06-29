3c4085e3a125611321bfe52dca57ad17f0feecd4
nswartwo <nswartwo@gmail.com>
2018-06-29 08:17:26 -0400

    temp def P file

---
+++ R/define_P.R
@@ -0,0 +1,13 @@
+###this script is to move this code into a place that I can find
+### it until I can decide the best place to move it into for final
+### model run
+
+load("data/ParamInit_2018.rData") # ParamInit
+P  <- ParamInit[,1];
+names(P) <- rownames(ParamInit)
+ii <-  ParamInit[,5]==1
+ParamInitZ <- ParamInit[ParamInit$Calib==1,]
+idZ0 <- ParamInitZ[,4]==0
+idZ1 <- ParamInitZ[,4]==1
+idZ2 <- ParamInitZ[,4]==2
+
