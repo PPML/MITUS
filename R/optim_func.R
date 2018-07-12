079cc1c9847ccf0b644b69d658774e3b37e6318b
nswartwo <nswartwo@gmail.com>
2018-07-12 13:47:41 -0400

    updates file name for Opt dataset

--- R/optim_func.R
+++ R/optim_func.R
@@ -32,23 +32,24 @@ if(min(dim(as.data.frame(df)))==1) {
 } else{
   df1 <- as.numeric(df[samp_i,])
 }
+
 b<-samp_i
 # for (i in min(b, nrow(StartVal))){
   o1  <- optim(df1, posterior, method ="BFGS", control=list(maxit=400,trace=5,reltol=sqrt(.Machine$double.eps)/5)) ; o1$value
-  save(o1,file=paste("Opt_US_r1_",b,"_new.rData",sep=""))
+  save(o1,file=paste("Opt_US_r1_", b,"_", Sys.Date(),".rda",sep=""))
   o2  <- optim(o1$par,posterior, method ="Nelder-Mead", control=list(maxit=1000,trace=1,reltol=sqrt(.Machine$double.eps)/5));  o2$value
-  save(o2,file=paste("Opt_US_r2_",b,"_new.rData",sep=""))
+  save(o2,file=paste("Opt_US_r2_", b,"_", Sys.Date(),".rda",sep=""))
   o3  <- optim(o2$par, posterior, method ="BFGS", control=list(maxit=400,trace=5,reltol=sqrt(.Machine$double.eps)/5)) ; o3$value
-  save(o3,file=paste("Opt_US_r3_",b,"_new.rData",sep=""))
+  save(o3,file=paste("Opt_US_r3_", b,"_", Sys.Date(),".rda",sep=""))
   o4  <- optim(o3$par ,posterior, method ="Nelder-Mead", control=list(maxit=1000,trace=1,reltol=sqrt(.Machine$double.eps)/5));  o4$value
-  save(o4,file=paste("Opt_US_r4_",b,"_new.rData",sep=""))
+  save(o4,file=paste("Opt_US_r4_", b,"_", Sys.Date(),".rda",sep=""))
   o5  <- optim(o4$par, posterior, method ="BFGS", control=list(maxit=400,trace=5,reltol=sqrt(.Machine$double.eps)/5)) ; o5$value
-  save(o5,file=paste("Opt_US_r5_",b,"_new.rData",sep=""))
+  save(o5,file=paste("Opt_US_r5_", b,"_", Sys.Date(),".rda",sep=""))
   o6  <- optim(o5$par ,posterior, method ="Nelder-Mead", control=list(maxit=1000,trace=1,reltol=sqrt(.Machine$double.eps)/5));  o6$value
-  save(o6,file=paste("Opt_US_r6_",b,"_new.rData",sep=""))
+  save(o6,file=paste("Opt_US_r6_", b,"_", Sys.Date(),".rda",sep=""))
   o7  <- optim(o6$par, posterior, method ="BFGS", control=list(maxit=400,trace=5,reltol=sqrt(.Machine$double.eps)/5)) ; o7$value
-  save(o7,file=paste("Opt_US_r7_",b,"_new.rData",sep=""))
+  save(o7,file=paste("Opt_US_r7_", b,"_", Sys.Date(),".rda",sep=""))
   o8 <- UnivOptim(o7$par) ; o8$value
-  save(o8,file=paste("Opt_US_r8_",b,"_new.rData",sep=""))
+  save(o8,file=paste("Opt_US_r8_", b,"_", Sys.Date(),".rda",sep=""))
 # }
 }
