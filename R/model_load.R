8c98a80a85953b89a373a2defb0607adcce5d0fc
nswartwo <nswartwo@gmail.com>
2018-08-06 17:23:25 -0400

    updates lazyload data

--- R/model_load.R
+++ R/model_load.R
@@ -1,13 +1,15 @@
-#' #'load initial conditions
-#' #'load necessary libraries
-#' library(mnormt)
-#' library(parallel)
-#' library(lhs)
-#' library(Rcpp)
-#' library(MCMCpack)
-#' library(MASS)
-#'
-#' #'lazy load necessary datasets
-#' #'Model Input
-#' data("ModelInputs_9-2-16", package = 'MITUS')
-#' data("ParamInitUS_2018-08-06_final", package='MITUS')# ParamInit
+#'load initial conditions
+#'load necessary libraries
+library(mnormt)
+library(parallel)
+library(lhs)
+library(Rcpp)
+library(MCMCpack)
+library(MASS)
+
+#'lazy load necessary datasets
+#'Model Input
+data("ModelInputs_9-2-16", package = 'MITUS')
+data("ParamInitUS_2018-08-06_final", package='MITUS')# ParamInit
+data("StartVal_2018-08-06", package='MITUS')# ParamInit
+data("CalibDat_2018-07-12", package='MITUS')# ParamInit
