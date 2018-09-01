ae15b469af57ec6287556fd925d127b68c5da9be
nswartwo <nswartwo@gmail.com>
2018-09-01 14:38:16 -0400

    def of P,ParamInitZ,&calibwts mv here

--- R/model_load.R
+++ R/model_load.R
@@ -11,5 +11,21 @@ library(MASS)
 #'Model Input
 data("ModelInputs_9-2-16", package = 'MITUS')
 data("ParamInitUS_2018-08-06_final", package='MITUS')# ParamInit
-data("StartVal_2018-08-23", package='MITUS')# ParamInit
+data("StartVal_2018-08-30", package='MITUS')# ParamInit
 data("CalibDat_2018-07-12", package='MITUS')# ParamInit
+
+#'creation of background parameters
+#'elements of P will be replaced from either the StartVals in the case
+#'of optimization or the user inputted dataset
+P  <- ParamInit[,1];
+names(P) <- rownames(ParamInit)
+ii <-  ParamInit[,5]==1
+ParamInitZ <- ParamInit[ParamInit$Calib==1,]
+idZ0 <- ParamInitZ[,4]==0
+idZ1 <- ParamInitZ[,4]==1
+idZ2 <- ParamInitZ[,4]==2
+
+#'format the calibration data
+wts <- CalibDat[["ImptWeights"]]
+CalibDat$US_tot_mort <- read.csv(file="inst/extdata/US_total_mort.csv", header = FALSE)
+CalibDat$US_mort_age <- read.csv(file="inst/extdata/US_mort_age.csv", header = TRUE)
