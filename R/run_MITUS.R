b75da30a7be586e4a40eae88114e2bbe440f27e9
nswartwo <nswartwo@gmail.com>
2018-09-07 17:29:11 -0400

    rm call to tabby formatting

--- R/run_MITUS.R
+++ R/run_MITUS.R
@@ -6,6 +6,6 @@
 run_MITUS <-function(loc="US"){
 model_load(loc)
 OutputsZint(1,P,startyr=1950,endyr=2050,Int1=0,Int2=0,Int3=0,Int4=0,Int5=0,Scen1=0,Scen2=0,Scen3=0)
-tabby_results(results)
-return(t_results)
+#tabby_results(results)
+return(results)
 }
