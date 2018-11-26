c9fe21d12ce6d150bb7adb2cc4bd1708aab33ac7
nswartwo <nswartwo@gmail.com>
2018-11-26 15:10:48 -0500

    updates data calls

--- R/model_load.R
+++ R/model_load.R
@@ -13,13 +13,15 @@ model_load<-function(loc="US"){
 #'Model Input
 if (loc=="US"){
   calib_dat<-paste0(loc,"_CalibDat_2018-09-17")
-
+  par_init<-paste0("US_ParamInit_2018-10-29")
+  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
+  start_val<-paste0("US_StartVal_2018-10-29")
 } else {
   calib_dat<-paste0("CalibDatState_7-2-18")
+  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
+  par_init<-paste0("ParamInit_st_2018-11-14")
+  start_val<-paste0("ST_StartVal_2018-11-14")
 }
-model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-par_init<-paste0("US_ParamInit_2018-10-31")
-start_val<-paste0("US_StartVal_2018-10-31")
 
 data(list=model_inputs, package = 'MITUS')
 data(list=par_init, package = 'MITUS')
@@ -28,25 +30,34 @@ data(list=calib_dat, package='MITUS')# ParamInit
 
 if (loc=="US"){
   wts <<- CalibDat[["ImptWeights"]]
+  P  <<- ParamInit[,1]
+  names(P) <<- rownames(ParamInit)
 
+  ii <<-  ParamInit[,5]==1
+  ParamInitZ <<- ParamInit[ParamInit$Calib==1,]
+  idZ0 <<- ParamInitZ[,4]==0
+  idZ1 <<- ParamInitZ[,4]==1
+  idZ2 <<- ParamInitZ[,4]==2
 } else {
   wts <<- CalibDatState[["ImptWeights"]]
-  wtZ <- wts[44:67];  wtZ["2016"] <- 4
-  wtZ<<-wtZ
+  W <- wts[44:67];  W["2016"] <- 4
+  wtZ <<-W
 
+  #creation of background parameters
+  #elements of P will be replaced from either the StartVals in the case
+  #of optimization or the user inputted dataset
+
+  P  <<- ParamInit_st[,1]
+  names(P) <<- rownames(ParamInit_st)
+  ii <<-  ParamInit_st[,5]==1
+  ParamInitZ <<- ParamInit_st[ParamInit_st$Calib==1,]
+  idZ0 <<- ParamInitZ[,4]==0
+  idZ1 <<- ParamInitZ[,4]==1
+  idZ2 <<- ParamInitZ[,4]==2
+  ParamInit<-ParamInit_st
 }
-#'creation of background parameters
-#'elements of P will be replaced from either the StartVals in the case
-#'of optimization or the user inputted dataset
-P  <<- ParamInit[,1]
-names(P) <<- rownames(ParamInit)
 
-ii <<-  ParamInit[,5]==1
-ParamInitZ <<- ParamInit[ParamInit$Calib==1,]
-idZ0 <<- ParamInitZ[,4]==0
-idZ1 <<- ParamInitZ[,4]==1
-idZ2 <<- ParamInitZ[,4]==2
-
+#
 #'format the calibration data
 # targets[["ParamInitZ"]]<-ParamInitZ
 # targets[["idZ0"]]<-idZ0
@@ -56,7 +67,6 @@ idZ2 <<- ParamInitZ[,4]==2
 
 return(invisible(NULL))
 }
-
 
 #'loads data for the geography of interest
 #'@name model_load_demo
