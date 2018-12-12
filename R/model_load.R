664e9c74e0efd814e6c1a7edc9b459cfa6dd9aca
nswartwo <nswartwo@gmail.com>
2018-12-12 14:04:32 -0500

    updates the data for fixed values at state level

--- R/model_load.R
+++ R/model_load.R
@@ -19,8 +19,8 @@ if (loc=="US"){
 } else {
   calib_dat<-paste0("CalibDatState_7-2-18")
   model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-  par_init<-paste0("ST_ParamInit_2018-12-10")
-  start_val<-paste0("ST_StartVal_2018-12-10")
+  par_init<-paste0("ST_ParamInit_2018-12-12")
+  start_val<-paste0("ST_StartVal_2018-12-12")
 }
 data(list=model_inputs, package = 'MITUS')
 data(list=par_init, package = 'MITUS')
