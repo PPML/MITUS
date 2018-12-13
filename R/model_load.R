aa453611ccfe03817347b031921309500218151a
nswartwo <nswartwo@gmail.com>
2018-12-13 14:04:16 -0500

    updates data so mort params are calib'd

--- R/model_load.R
+++ R/model_load.R
@@ -19,8 +19,8 @@ if (loc=="US"){
 } else {
   calib_dat<-paste0("CalibDatState_7-2-18")
   model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-  par_init<-paste0("ST_ParamInit_2018-12-12")
-  start_val<-paste0("ST_StartVal_2018-12-12")
+  par_init<-paste0("ST_ParamInit_2018-12-13")
+  start_val<-paste0("ST_StartVal_2018-12-13")
 }
 data(list=model_inputs, package = 'MITUS')
 data(list=par_init, package = 'MITUS')
