c60045bc5ffe6964cca15dce04b3f25f38fb80b5
nswartwo <nswartwo@gmail.com>
2019-02-07 14:22:19 -0500

    adding func

---
+++ R/on_the_cluster.R
@@ -0,0 +1,9 @@
+#' A Function to Test if we're on the Harvard cluster.
+#'@name on_the_cluster
+#'@export
+on_the_cluster <- function() {
+  e <- Sys.info()
+  return(
+    grepl("Linux", e[['sysname']]) &&
+      grepl("harvard.edu", e[['nodename']]))
+}
