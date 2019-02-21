59b5811c21c738d8fcb5977e1b25619b59a14aec
Christian Testa <testa101@gmail.com>
2019-02-21 15:50:40 -0500

    Un-delete the model_load_demo function.

--- R/model_load.R
+++ R/model_load.R
@@ -71,3 +71,66 @@ model_load<-function(loc="US", return_env=F){
 		}
 	}
 }
+
+#'loads data for the geography of interest
+#'@name model_load_demo
+#'@param loc two letter postal abbreviation for states; US for national
+#'@return void
+model_load_demo<-function(loc="US"){
+  library(mnormt)
+  library(parallel)
+  library(lhs)
+  library(Rcpp)
+  library(MCMCpack)
+  library(MASS)
+  #'lazy load necessary datasets
+  #'Model Input
+  if (loc=="US")
+  {
+    calib_dat<-paste0(loc,"_CalibDat_2018-09-17")
+
+  } else {
+    calib_dat<-paste0("CalibDatState_7-2-18")
+
+  }
+  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
+  par_init<-paste0("US_ParamInit_demo")
+  start_val<-paste0("US_StartVal_demo")
+
+  data(list=model_inputs, package = 'MITUS')
+  data(list=par_init, package = 'MITUS')
+  data(list=start_val, package = 'MITUS')
+  data(list=calib_dat, package='MITUS')# ParamInit
+
+  if (loc=="US")
+  {
+    wts <<- CalibDat[["ImptWeights"]]
+
+  } else {
+    wts <<- CalibDatState[["ImptWeights"]]
+    wtZ <- wts[44:67];  wtZ["2016"] <- 4
+    wtZ<<-wtZ
+
+  }
+  #'creation of background parameters
+  #'elements of P will be replaced from either the StartVals in the case
+  #'of optimization or the user inputted dataset
+  #'
+  P  <<- ParamInit_demo[,1]
+  names(P) <<- rownames(ParamInit_demo)
+
+  ii <<-  ParamInit_demo[,5]==1
+  ParamInitZ <<- ParamInit_demo[ParamInit_demo$Calib==1,]
+  idZ0 <<- ParamInitZ[,4]==0
+  idZ1 <<- ParamInitZ[,4]==1
+  idZ2 <<- ParamInitZ[,4]==2
+
+  #'format the calibration data
+  # targets[["ParamInitZ"]]<-ParamInitZ
+  # targets[["idZ0"]]<-idZ0
+  # targets[["idZ1"]]<-idZ1
+  # targets[["idZ2"]]<-idZ2
+  # targets[["P"]]<-P
+
+  return(invisible(NULL))
+}
