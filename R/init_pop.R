03e7e913f8acca2247e38ce7e4479743c0dea507
nswartwo <nswartwo@gmail.com>
2019-03-14 13:08:35 -0400

    updates initpop data call

--- R/init_pop.R
+++ R/init_pop.R
@@ -1,11 +1,10 @@
 init_pop<-function(){
-  InitPopN<-as.matrix(read.csv("~/Desktop/NCHS_initpop.csv", header = TRUE )[,3:4])/1e6
-  row.names(InitPopN) = read.csv("~/Desktop/NCHS_initpop.csv")[,1]
+  InitPopN<-readRDS(system.file("US/NCHS_initpop.rds", package="MITUS"))
   InitPopN<-as.matrix(InitPopN)
   return(InitPopN)
 }
 
-NCHS_mort<-c(754.593,60.1,128.1,178.7,358.7,853.9,1901,4104.4,9331.1,20196.9,20196.9)/1e5
+# NCHS_mort<-c(754.593,60.1,128.1,178.7,358.7,853.9,1901,4104.4,9331.1,20196.9,20196.9)/1e5
 
 # NCHS_mort<-as.matrix(read.csv("~/Desktop/NCHS_BgMort.csv",header = TRUE))[,2:12]
 # row.names(NCHS_mort)=read.csv("~/Desktop/NCHS_BgMort.csv")[,1]
