df1b67795abe3b036fdbd10b9fbf8a2c62fc4176
nswartwo <nswartwo@gmail.com>
2019-04-10 09:18:39 -0400

    first draft of tabby interfacing functions

---
+++ R/optim_load.R
@@ -0,0 +1,13 @@
+#'loads data for the geography of interest
+#'@name optim_load
+#'@return env
+optim_load<-function(){
+  optim_vec<-rep("F",51)
+  if(length(i <- grep(output, colnames(df),ignore.case = TRUE)))
+
+x<-"~/MITUS/inst/CA/CA_Optim_all_10_2019-02-13.rds"
+for (loc in stateID[,3]){
+  if (grep(paste0(loc,"_Optim_all"))==TRUE){
+  optim_vec[]
+}}
+}
---
+++ R/tabby_load.R
@@ -0,0 +1,56 @@
+#'loads data for the geography of interest
+#'@name tabby_load
+#'@param loc two letter postal abbreviation for states; US for national
+#'@return env
+tabby_load<-function(loc="US"){
+  library(mnormt)
+  library(parallel)
+  library(lhs)
+  library(Rcpp)
+  library(MCMCpack)
+  library(MASS)
+  #'load necessary datasets
+  #'Model Input
+  if (loc=="US"){
+    CalibDat<<-readRDS(system.file("US/US_CalibDat_03-06-19.rds", package="MITUS"))
+    ParamInit<<-readRDS(system.file("US/US_ParamInit_01-24-19.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file("US/US_Inputs_01-24-19.rds", package="MITUS"))
+
+  } else {
+    CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_01-24-19.rds", package="MITUS"))
+    ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_02-09-19.rds", package="MITUS"))
+    StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_02-09-19.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_01-24-19.rds"), package="MITUS"))
+  }
+
+  if (loc=="US"){
+    wts <<- CalibDat[["ImptWeights"]]
+    P  <<- ParamInit[,1]
+    names(P) <<- rownames(ParamInit)
+
+    ii <<-  ParamInit[,5]==1
+    ParamInitZ <<- ParamInit[ParamInit$Calib==1,]
+    idZ0 <<- ParamInitZ[,4]==0
+    idZ1 <<- ParamInitZ[,4]==1
+    idZ2 <<- ParamInitZ[,4]==2
+  } else {
+    wts <<- CalibDatState[["ImptWeights"]]
+    W <- wts[44:67];  W["2016"] <- 4
+    wtZ <<-W
+
+    #creation of background parameters
+    #elements of P will be replaced from either the StartVals in the case
+    #of optimization or the user inputted dataset
+
+    P  <<- ParamInit_st[,1]
+    names(P) <<- rownames(ParamInit_st)
+    ii <<-  ParamInit_st[,5]==1
+    ParamInitZ <<- ParamInit_st[ParamInit_st$Calib==1,]
+    idZ0 <<- ParamInitZ[,4]==0
+    idZ1 <<- ParamInitZ[,4]==1
+    idZ2 <<- ParamInitZ[,4]==2
+    ParamInit<-ParamInit_st
+  }
+
+  return(env)
+}
