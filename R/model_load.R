bb81414d0043f155729245f477ed2247ac72a0a3
nswartwo <nswartwo@gmail.com>
2019-04-22 15:03:52 -0400

    new inputs and model_load update --Nov 27,2018 optims

--- R/model_load.R
+++ R/model_load.R
@@ -9,63 +9,52 @@ model_load<-function(loc="US"){
   library(Rcpp)
   library(MCMCpack)
   library(MASS)
-#'lazy load necessary datasets
-#'Model Input
-if (loc=="US"){
-  calib_dat<-paste0(loc,"_CalibDat_2018-09-17")
-  par_init<-paste0("US_ParamInit_2018-10-29")
-  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-  start_val<-paste0("US_StartVal_2018-10-29")
-} else {
-  calib_dat<-paste0("CalibDatState_7-2-18")
-  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-  par_init<-paste0("ParamInit_st_2018-11-14")
-  start_val<-paste0("ST_StartVal_2018-11-14")
-}
+  #'load necessary datasets
+  #'Model Input
+  if (loc=="US"){
+    CalibDat<<-readRDS(system.file("US/US_CalibDat_2018-09-17.rds", package="MITUS"))
+    ParamInit<<-readRDS(system.file("US/US_ParamInit_2018-12-05.rds", package="MITUS"))
+    StartVal<<-readRDS(system.file("US/US_StartVal_2018-12-05.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file("US/US_Inputs_12-05-2018.rds", package="MITUS"))
 
-data(list=model_inputs, package = 'MITUS')
-data(list=par_init, package = 'MITUS')
-data(list=start_val, package = 'MITUS')
-data(list=calib_dat, package='MITUS')# ParamInit
+  } else {
+    CalibDat<<-CalibDatState<<-readRDS(system.file("ST/CalibDatState_7-2-18.rds", package="MITUS"))
+    ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2018-12-10.rds", package="MITUS"))
+    StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2018-12-10.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_01-24-19.rds"), package="MITUS"))
+    Opt <<- readRDS(system.file(paste0(loc,"/",loc,"_opt_all_2018-12-12.rds"), package="MITUS"))
+  }
 
-if (loc=="US"){
-  wts <<- CalibDat[["ImptWeights"]]
-  P  <<- ParamInit[,1]
-  names(P) <<- rownames(ParamInit)
+  if (loc=="US"){
+    wts <<- CalibDat[["ImptWeights"]]
+    P  <<- ParamInit[,1]
+    names(P) <<- rownames(ParamInit)
 
-  ii <<-  ParamInit[,5]==1
-  ParamInitZ <<- ParamInit[ParamInit$Calib==1,]
-  idZ0 <<- ParamInitZ[,4]==0
-  idZ1 <<- ParamInitZ[,4]==1
-  idZ2 <<- ParamInitZ[,4]==2
-} else {
-  wts <<- CalibDatState[["ImptWeights"]]
-  W <- wts[44:67];  W["2016"] <- 4
-  wtZ <<-W
+    ii <<-  ParamInit[,5]==1
+    ParamInitZ <<- ParamInit[ParamInit$Calib==1,]
+    idZ0 <<- ParamInitZ[,4]==0
+    idZ1 <<- ParamInitZ[,4]==1
+    idZ2 <<- ParamInitZ[,4]==2
+  } else {
+    wts <<- CalibDatState[["ImptWeights"]]
+    W <- wts[44:67];  W["2016"] <- 4
+    wtZ <<-W
 
-  #creation of background parameters
-  #elements of P will be replaced from either the StartVals in the case
-  #of optimization or the user inputted dataset
+    #creation of background parameters
+    #elements of P will be replaced from either the StartVals in the case
+    #of optimization or the user inputted dataset
 
-  P  <<- ParamInit_st[,1]
-  names(P) <<- rownames(ParamInit_st)
-  ii <<-  ParamInit_st[,5]==1
-  ParamInitZ <<- ParamInit_st[ParamInit_st$Calib==1,]
-  idZ0 <<- ParamInitZ[,4]==0
-  idZ1 <<- ParamInitZ[,4]==1
-  idZ2 <<- ParamInitZ[,4]==2
-  ParamInit<-ParamInit_st
-}
+    P  <<- ParamInit_st[,1]
+    names(P) <<- rownames(ParamInit_st)
+    ii <<-  ParamInit_st[,5]==1
+    ParamInitZ <<- ParamInit_st[ParamInit_st$Calib==1,]
+    idZ0 <<- ParamInitZ[,4]==0
+    idZ1 <<- ParamInitZ[,4]==1
+    idZ2 <<- ParamInitZ[,4]==2
+    ParamInit<-ParamInit_st
+  }
 
-#
-#'format the calibration data
-# targets[["ParamInitZ"]]<-ParamInitZ
-# targets[["idZ0"]]<-idZ0
-# targets[["idZ1"]]<-idZ1
-# targets[["idZ2"]]<-idZ2
-# targets[["P"]]<-P
-
-return(invisible(NULL))
+  return(invisible(NULL))
 }
 
 #'loads data for the geography of interest
@@ -81,22 +70,20 @@ model_load_demo<-function(loc="US"){
   library(MASS)
   #'lazy load necessary datasets
   #'Model Input
-  if (loc=="US")
-  {
-    calib_dat<-paste0(loc,"_CalibDat_2018-09-17")
+  if (loc=="US"){
+    CalibDat<<-readRDS(system.file("US/US_CalibDat_03-06-19.rds", package="MITUS"))
+
+    # CalibDat<<-readRDS(system.file("US/US_CalibDat_02-14-19.rds", package="MITUS"))
+    ParamInit_demo<<-readRDS(system.file("US/US_ParamInitdemo_2019-02-21.rds", package="MITUS"))
+    StartVal_demo<<-readRDS(system.file("US/US_StartValdemo_2019-02-21.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file("US/US_Inputs_01-24-19.rds", package="MITUS"))
 
   } else {
-    calib_dat<-paste0("CalibDatState_7-2-18")
-
+    CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_01-24-19.rds", package="MITUS"))
+    ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_02-09-19.rds", package="MITUS"))
+    StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_02-09-19.rds", package="MITUS"))
+    Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_01-24-19.rds"), package="MITUS"))
   }
-  model_inputs<-paste0(loc,"_ModelInputs_11-13-18")
-  par_init<-paste0("US_ParamInit_demo")
-  start_val<-paste0("US_StartVal_demo")
-
-  data(list=model_inputs, package = 'MITUS')
-  data(list=par_init, package = 'MITUS')
-  data(list=start_val, package = 'MITUS')
-  data(list=calib_dat, package='MITUS')# ParamInit
 
   if (loc=="US")
   {
