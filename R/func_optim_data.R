06626df72c506fe9984a834df09d09af81e1d5af
nswartwo <nswartwo@gmail.com>
2019-05-29 14:34:30 -0400

    new optim data -- no good

--- R/func_optim_data.R
+++ R/func_optim_data.R
@@ -17,13 +17,13 @@ rnames[j]<-paste("b_no_", j, sep="")
 }
 rownames(opt_all)<-rnames
 
-for (i in c(1:3,5:10)){
-  load(paste("/Users/nis100/Desktop/US_320/Opt_US_r7_",i,"_2019-03-19.rda", sep=""))
+for (i in c(1:5,7:10)){
+  load(paste("/Users/nis100/Desktop/US_528/Opt_US_r7_",i,"_2019-05-28.rda", sep=""))
   opt_all[i,1:nrow(ParamInitZ)] <- o7$par
   opt_all[i,nrow(ParamInitZ)+1]<- o7$value
 }
 US_opt_all<-opt_all
-saveRDS(US_opt_all, file=paste("~/MITUS/inst/US/US_Optim_all_", batches,"_", Sys.Date(),".rds", sep = ""))
+saveRDS(US_opt_all, file=paste("~/MITUS/inst/US/US_Optim_all_", batches,"_528.rds", sep = ""))
 
 }
 
