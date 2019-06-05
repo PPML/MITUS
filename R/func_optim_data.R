63241ab2399b6a832aaa34e83a9af79018b386f3
nswartwo <nswartwo@gmail.com>
2019-06-05 11:48:14 -0400

    new optim data; gives clear mode; does not hit mort

--- R/func_optim_data.R
+++ R/func_optim_data.R
@@ -18,12 +18,12 @@ rnames[j]<-paste("b_no_", j, sep="")
 rownames(opt_all)<-rnames
 
 for (i in 1:batches){
-  load(paste("/Users/nis100/Desktop/US_603/Opt_US_r7_",i,"_2019-06-03.rda", sep=""))
+  load(paste("/Users/nis100/Desktop/US_604/Opt_US_r7_",i,"_2019-06-04.rda", sep=""))
   opt_all[i,1:nrow(ParamInitZ)] <- o7$par
   opt_all[i,nrow(ParamInitZ)+1]<- o7$value
 }
 US_opt_all<-opt_all
-saveRDS(US_opt_all, file=paste("~/MITUS/inst/US/US_Optim_all_", batches,"_603.rds", sep = ""))
+saveRDS(US_opt_all, file=paste("~/MITUS/inst/US/US_Optim_all_", batches,"_604.rds", sep = ""))
 
 }
 
