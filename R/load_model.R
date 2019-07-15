df4db7a19d2219a2a50e11c6a5b3289d0cfca41e
nswartwo <nswartwo@gmail.com>
2019-07-15 13:47:36 -0400

    ensures all calibdat is numeric

--- R/load_model.R
+++ R/load_model.R
@@ -20,7 +20,7 @@ if (loc=="US"){
   Inputs<<-readRDS(system.file("US/US_Inputs_06-26-19.rds", package="MITUS"))
 
 } else {
-  CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_07-03-19.rds", package="MITUS"))
+  CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_07-15-19.rds", package="MITUS"))
   ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2019-07-12.rds", package="MITUS"))
   StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2019-07-12.rds", package="MITUS"))
   Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_07-03-19.rds"), package="MITUS"))
