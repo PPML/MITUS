ecb2771c541f6b8e5e2a194020a2974e09f75081
nswartwo <nswartwo@gmail.com>
2019-08-01 10:39:37 -0400

    rms old files

--- R/bezier.R
+++
@@ -1,8 +0,0 @@
-bezier_fun<-function(mortality.bezier){
-  bez.a <- mortality.bezier[1]
-  bez.b <- mortality.bezier[2]
-  bez.c <- mortality.bezier[3]
-  bez.d <- mortality.bezier[4]
-
-
-  }
--- R/on_the_cluster.R
+++
@@ -1,9 +0,0 @@
-#' A Function to Test if we're on the Harvard cluster.
-#'@name on_the_cluster
-#'@export
-on_the_cluster <- function() {
-  e <- Sys.info()
-  return(
-    grepl("Linux", e[['sysname']]) &&
-      grepl("harvard.edu", e[['nodename']]))
-}
