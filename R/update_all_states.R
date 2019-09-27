672f4e530788af4f6b57f0a0c892fff034b74707
nswartwo <nswartwo@gmail.com>
2019-09-27 15:09:03 -0400

    adds us to loc vec

--- R/update_all_states.R
+++ R/update_all_states.R
@@ -1,4 +1,4 @@
-vector_all_locs<-c("CA", "MA", "FL", "GA", "IL", "TX", "NY", "NJ", "PA", "WA", "VA")
+vector_all_locs<-c("US","CA", "MA", "FL", "GA", "IL", "TX", "NY", "NJ", "PA", "WA", "VA" )
 
 update_all_states<-function(loc_vec, simp.date){
   for (i in 1:length(loc_vec)){
