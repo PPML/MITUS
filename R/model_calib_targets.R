2a64e12a2c0b4caae66eab0fc01eeea8821662a6
nswartwo <nswartwo@gmail.com>
2019-09-30 15:38:31 -0400

    rms unnecessary files

--- R/model_calib_targets.R
+++ R/model_calib_targets.R
@@ -1,157 +1,180 @@
-#' THIS FUNCTION READS IN THE BASE CASE SCENARIO AND THEN
-#' CREATES INDIVIDUAL RDS FILES FOR OUTPUTS NECESSARY FOR
-#' THE TABBY2 COMPARISON TO RECENT DATA PLOTS
+# ##format calibration data for tabby2 plots
+# ##united states
+# NCD<-readRDS(system.file("US/US_CalibDat_03-06-19.rds", package="MITUS"))
+#
+# #states
+# SCD<-readRDS(system.file("ST/ST_CalibDat_07-03-19.rds", package="MITUS"))
+#
+# for(i in 1:length(NCD)){
+#   x<-NCD[[i]]
+#   saveRDS(x,file = paste0("~/MITUS/inst/US/calibration_targets/US_",names(NCD)[i],"_03-06-19.rds"))
+# }
+#
+# for (i in 1:length(SCD)){
+#   x<-SCD[[i]];
+#   # if (length(x)==51){
+#   for (st in 1:51){
+#   y<-x[st,]
+#   loc2<-stateID[st,3]
+#   saveRDS(y,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_",names(SCD)[i],"_07-03-19.rds"))
+#   }
+#
+#
+#   else {
+#   saveRDS(x,file = paste0("~/MITUS/inst/ST/calibration_targets/ST_",names(SCD)[i],"_07-03-19.rds"))
+#   }
+#   }
+# }
+#
+##manual reformatting of state data to match national data
+#decennial population
+# for (st in 1:51){
+#   x<-SCD[["pop_50_10"]][[st]]
+#   loc2<-stateID[st,3]
+#   pop_yr_fb<-matrix(NA,7,4);
+#   colnames(pop_yr_fb)<-c("year","total","native","foreign.born")
+#   pop_yr_fb[,1]<-seq(1950,2010,10)
+#   pop_yr_fb[,2]<-colSums(x[,3:9])/1e6
+#   pop_yr_fb[,3]<-colSums(x[x$usb==1,3:9])/1e6
+#   pop_yr_fb[,4]<-colSums(x[x$usb==0,3:9])/1e6
+#   saveRDS(pop_yr_fb,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_tot_pop_yr_fb_07-03-19.rds"))
+# }
 
-#'@name model_calib_targets
-#'@param loc two character location code for the model location
-#'@param bc.array base case results array
-#'@param samp.i which simulation to use (1:10)
-#'@param simp.date date to append to the files; should match the optim date
-#'@export
+#population by age
+# for (st in 1:51){
+#   x<-SCD[["pop_00_17"]][[st]]
+#   #subset this to only 2016
+#   x<-x[,c(1,2,19)]
+#   loc2<-stateID[st,3]
+#   tot_pop16_ag_fb<-matrix(NA,9,4);
+#   tot_pop16_ag_fb<-as.data.frame(tot_pop16_ag_fb)
+#   colnames(tot_pop16_ag_fb)<-c("age_grp","total","us","fb")
+#   tot_pop16_ag_fb[,1]<-as.factor(c("0_4", "5_24","25_44","45_54","55_64","65_74", "75_84", "85p", "all"))
+#   tot_pop16_ag_fb[1,2:4]<-cbind(sum(x[x$age_group==1,3]), x[(x$age_group==1 & x$usb==1),3], x[x$age_group==1 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[2,2:4]<-cbind(sum(x[(x$age_group==2 | x$age_group==3),3]), sum(x[((x$age_group==2 | x$age_group==3)  & x$usb==1),3]), sum(x[(x$age_group==2 | x$age_group==3) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[3,2:4]<-cbind(sum(x[(x$age_group==4 | x$age_group==5),3]), sum(x[((x$age_group==4 | x$age_group==5)  & x$usb==1),3]), sum(x[(x$age_group==4 | x$age_group==5) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[4,2:4]<-cbind(sum(x[x$age_group==6,3]), x[(x$age_group==6 & x$usb==1),3], x[x$age_group==6 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[5,2:4]<-cbind(sum(x[x$age_group==7,3]), x[(x$age_group==7 & x$usb==1),3], x[x$age_group==7 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[6,2:4]<-cbind(sum(x[x$age_group==8,3]), x[(x$age_group==8 & x$usb==1),3], x[x$age_group==8 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[7,2:4]<-cbind(sum(x[x$age_group==9,3]), x[(x$age_group==9 & x$usb==1),3], x[x$age_group==9 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[8,2:4]<-cbind(sum(x[(x$age_group==10 | x$age_group==11),3]), sum(x[((x$age_group==10 | x$age_group==11)  & x$usb==1),3]), sum(x[(x$age_group==10 | x$age_group==11) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[9,2:4]<-cbind(sum(x[,3]), sum(x[(x$usb==1),3]), sum(x[x$usb==0,3]))/1e6
+#
+#   saveRDS(tot_pop16_ag_fb,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_tot_pop16_ag_fb_07-03-19.rds"))
+# }
 
-model_calib_targets<-function(loc="US"){
-  model_load("loc")
-  ############                   demographic targets                     ############
-  ### ### ### ### ### ###   TOTAL POP EACH DECADE, BY US/FB   ### ### ### ### ### ###
-  V  <- cbind(res[1:68,30], res[1:68,31]+res[1:68,32])
-  #read in decade based stuff
-  tot_pop<- CalibDatState[["pop_50_10"]][[st]]
-  #get the FB pop from the decade
-  tot_pop_yr_fb   <- tot_pop[tot_pop[,2]==0,]
-  #get 2017 population
-  pop_ag_11_170  <- CalibDatState[["pop_00_17"]][[st]][,c(1,2,20)]
-  #get 2017 fb population
-  pop_ag_11_17nus <-sum(pop_ag_11_170[pop_ag_11_170[,2]==0,3][-11])
-  #append the foreign born population
-  tot_pop_yr_nus  <- c(colSums(tot_pop_yr_fb[,-c(1:2)]), pop_ag_11_17nus)/1e6
+# for (st in 1:51){
+#   x<-SCD[["pop_00_17"]][[st]]
+#   #subset this to only 2016
+#   x<-x[,c(1,2,19)]
+#   loc2<-stateID[st,3]
+#   tot_pop16_ag_fb<-matrix(NA,9,4);
+#   tot_pop16_ag_fb<-as.data.frame(tot_pop16_ag_fb)
+#   colnames(tot_pop16_ag_fb)<-c("age_grp","total","us","fb")
+#   tot_pop16_ag_fb[,1]<-as.factor(c("0_4", "5_24","25_44","45_54","55_64","65_74", "75_84", "85p", "all"))
+#   tot_pop16_ag_fb[1,2:4]<-cbind(sum(x[x$age_group==1,3]), x[(x$age_group==1 & x$usb==1),3], x[x$age_group==1 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[2,2:4]<-cbind(sum(x[(x$age_group==2 | x$age_group==3),3]), sum(x[((x$age_group==2 | x$age_group==3)  & x$usb==1),3]), sum(x[(x$age_group==2 | x$age_group==3) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[3,2:4]<-cbind(sum(x[(x$age_group==4 | x$age_group==5),3]), sum(x[((x$age_group==4 | x$age_group==5)  & x$usb==1),3]), sum(x[(x$age_group==4 | x$age_group==5) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[4,2:4]<-cbind(sum(x[x$age_group==6,3]), x[(x$age_group==6 & x$usb==1),3], x[x$age_group==6 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[5,2:4]<-cbind(sum(x[x$age_group==7,3]), x[(x$age_group==7 & x$usb==1),3], x[x$age_group==7 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[6,2:4]<-cbind(sum(x[x$age_group==8,3]), x[(x$age_group==8 & x$usb==1),3], x[x$age_group==8 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[7,2:4]<-cbind(sum(x[x$age_group==9,3]), x[(x$age_group==9 & x$usb==1),3], x[x$age_group==9 & x$usb==0,3])/1e6
+#   tot_pop16_ag_fb[8,2:4]<-cbind(sum(x[(x$age_group==10 | x$age_group==11),3]), sum(x[((x$age_group==10 | x$age_group==11)  & x$usb==1),3]), sum(x[(x$age_group==10 | x$age_group==11) & x$usb==0,3]))/1e6
+#   tot_pop16_ag_fb[9,2:4]<-cbind(sum(x[,3]), sum(x[(x$usb==1),3]), sum(x[x$usb==0,3]))/1e6
+#
+#   saveRDS(tot_pop16_ag_fb,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_tot_pop16_ag_fb_07-03-19.rds"))
+# }
 
-  #get the FB pop from the decade
-  tot_pop_yr_us   <- tot_pop[tot_pop[,2]==1,]
-  #get 2017 population
-  pop_ag_11_170  <- CalibDatState[["pop_00_17"]][[st]][,c(1,2,20)]
-  #get 2017 fb population
-  pop_ag_11_17us <-sum(pop_ag_11_170[pop_ag_11_170[,2]==1,3][-11])
-  #append the us born population
-  tot_pop_yr_us   <- c(colSums(tot_pop_yr_us[,-c(1:2)]), pop_ag_11_17us)/1e6
-  V<-cbind(tot_pop_yr_us, tot_pop_yr_nus)
-  rownames(V)[8]<-"2017"
-  saveRDS(V,file = paste0("~/MITUS/inst/",loc,"/calibration_target/",loc,"_pop_yr_nat_",simp.date,".rds"))
-  ### ### ### ### ### ### TOTAL POP AGE DISTRIBUTION 2014  ### ### ### ### ### ###
-  V  <- cbind(t(res[68,33:43]), t(res[68,44:54]))
-  V1  <- V[-3,]
-  V1[2,] <- V1[2,]+V[3,]
-  V2 <- V1[-4,]
-  V2[3,] <- V2[3,]+V1[4,]
-  V3 <- V2[-9,]
-  V3[8,] <- V3[8,]+V2[9,]
-  saveRDS(V3,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_pop_ag_nat_",simp.date,".rds"))
+# fb case
+# for (st in 1:51){
+#   loc2<-stateID[st,3]
+#   x<-SCD[["cases_yr_ag_nat_st"]][[i]][,c(1,12),"nusb"]
+#   y<-SCD[["cases_yr_ag_nat_st"]][[i]][,12,"nusb"]+SCD[["cases_yr_ag_nat_st"]][[i]][,12,"usb"]
+#   yy<-x
+#   yy[,2]<-x[,2]/y
+#   yy<-cbind(yy,y)
+#   colnames(yy)<-c("year","pct_fb","sample_size")
+#   saveRDS(yy,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_fb_cases_07-03-19.rds"))
+# }
 
-  ### ### ### ### ### ###   TOTAL MORT EACH DECADE, BY US/FB  ### ### ### ### ### ###
-  V  <- cbind(rowSums(res[1:67,255:265]), rowSums(res[1:67,266:276]))
-  V1c <- rowSums(res[1:67,121:131])
-  saveRDS(V1c,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_mort_yr_nat_",simp.date,".rds"))
-
-  ### ### ### ### ### ###   TOTAL MORT AGE DISTRIBUTION 2014  ### ### ### ### ### ###
-  V  <- cbind((res[67,255:265])+(res[67,266:276]))
-  V1  <- V[,-3]
-  V1[,2] <- V1[,2]+V[,3]
-  V2 <- V1[,-4]
-  V2[,3] <- V2[,3]+V1[,4]
-  V3 <- V2[,-9]
-  V3[,8] <- V3[,8]+V2[,9]
-  saveRDS(V3,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_mort_ag_nat_",simp.date,".rds"))
-
-  ############                   tb specific targets                     ############
-  # graph of total diagnosed cases
-  # by total population, US born population, and non-US born population
-  V0 <- res[4:68,"NOTIF_ALL"]+res[4:68,"NOTIF_MORT_ALL"] #total population
-  V1 <- res[44:68,"NOTIF_US"]+res[44:68,"NOTIF_MORT_US"]   #US born population
-  V2 <- res[44:68,"NOTIF_F1"]+res[44:68,"NOTIF_F2"]+res[44:68,"NOTIF_MORT_F1"]+res[44:68,"NOTIF_MORT_F2"]   #non-US born population
-  tot_cases<-list()
-  tot_cases[["allpop"]]<-V0
-  tot_cases[["USBpop"]]<-V1
-  tot_cases[["NUSBpop"]]<-V2
-  saveRDS(tot_cases,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_TBcases_",simp.date,".rds"))
-
-  #Percent of Total Cases Non-US Born Population
-  V <- cbind(res[44:67,"NOTIF_US"]+res[44:67,"NOTIF_MORT_US"], #US born population
-             res[44:67,"NOTIF_F1"]+res[44:67,"NOTIF_F2"]+  #non-US born population
-               res[44:67,"NOTIF_MORT_F1"]+res[44:67,"NOTIF_MORT_F2"])
-  V <- V[,2]/rowSums(V)
-  #Percent of Non-US Born Cases from Recent Immigrant Population
-  V <- cbind(res[44:65,"NOTIF_F1"]+res[44:65,"NOTIF_MORT_F1"],res[44:65,"NOTIF_F2"]+res[44:65,"NOTIF_MORT_F2"])
-  V <- V[,1]/rowSums(V)*100
-  saveRDS(V,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_percentRecentFBcases_",simp.date,".rds"))
-
-  #Age distribution of Cases
-  #0-24 yrs, 25-44 yrs, 45-64 yrs, 65+ yrs
-  #ends in 2016
-  V   <- (res[51:67,136:146]+res[51:67,189:199])
-  V2<-matrix(NA,length(51:67),4)
-  V2[,1]<-rowSums(V[,1:3]); V2[,2]<-rowSums(V[,4:5])
-  V2[,3]<-rowSums(V[,6:8]); V2[,4]<-rowSums(V[,9:11])
-  saveRDS(V2*1e6,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_age_cases_4grps_",simp.date,".rds"))
-  #Age distribution of Cases
-  #all age bands
-  V   <- (res[51:67,136:146]+res[51:67,189:199])
-  V2  <- cbind(2000:2016,V)
-  saveRDS(V2,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_age_cases_tot_",simp.date,".rds"))
-
-  # Treatment Outcomes 1993-2014
-  V   <- res[44:65,132:134]
-  Vdisc <- V[,2]/rowSums(V)
-  Vdead <- V[,3]/rowSums(V)
-
-  txoutcomes<-list()
-  txoutcomes[["discontinued tx"]]<-Vdisc
-  txoutcomes[["died on tx"]]<-Vdead
-
-  saveRDS(txoutcomes,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_txOutcomes_",simp.date,".rds"))
-
-
-  #LTBI Prevalance by Age in 2011, US born
-  V  <- cbind(t(res[62,55:65]),t(res[62,33:43]-res[62,55:65]))
-  pIGRA<-1
-  v1<-V*pIGRA
-  Sens_IGRA <-c(.780,.675,.712,.789,.591)
-  Spec_IGRA <-c(.979,.958,.989,.985,.931)
-  names(Sens_IGRA)<- names(Spec_IGRA)<-c("lrUS","hrUS","youngNUS","NUS","hrNUS")
-  Va <- outer(v1[,1],c(Sens_IGRA[1],(1-Sens_IGRA[1])))+outer(v1[,2],c((1-Spec_IGRA[1]),Spec_IGRA[1]))
-  V1 <- Va[-11,]; V1<-V1[-10,]
-  V1[9,] <- V1[9,]+Va[10,]+Va[11,]
-  V2 <- rep(NA,8)
-  V2 <- V1[2:9,1]/rowSums(V1[2:9,])*100
-  # colnames(V2) <- c("LTBI", "No-LTBI")
-
-  saveRDS(V2,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_USB_LTBI_pct_",simp.date,".rds"))
-
-  #LTBI Prevalance by Age in 2011, non-US born
-  V  <- cbind(t(res[62,66:76]),t(res[62,44:54]-res[62,66:76]))
-  pIGRA<-1
-  v1<-V*pIGRA
-  #under age 5
-  v1b <- (v1[1,1]*c(Sens_IGRA[3],(1-Sens_IGRA[3])))+(v1[1,2]*c((1-Spec_IGRA[3]),Spec_IGRA[3]))
-  #over age 5
-  v1c <- outer(v1[2:11,1],c(Sens_IGRA[4],(1-Sens_IGRA[4])))+outer(v1[2:11,2],c((1-Spec_IGRA[4]),Spec_IGRA[4]))
-  v1d<-rbind(v1b,v1c)
-  colnames(v1d) <- c("LTBI", "No-LTBI")
-  V1 <- v1d[-11,]; V1<-V1[-10,]
-  V1[9,] <- V[9,]+v1d[10,]+v1d[11,]
-  V2 <- rep(NA,8)
-  V2 <- V1[2:9,1]/rowSums(V1[2:9,])*100
-  saveRDS(V2,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_NUSB_LTBI_pct_",simp.date,".rds"))
-
-  # Age Distribution of TB Deaths 1999-2014
-
-  V  <- res[50:65,227:237]
-  V2 <- V[,-11]; V2[,10] <- V[,10]+V[,11]
-  V3 <- colSums(V2)*1e6
-
-  saveRDS(V3,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_TBdeathsAge_",simp.date,".rds"))
-
-  # total tb deaths over time 2004-2014
-  V   <- rowSums(res[55:65,227:237])
-  saveRDS(V,file = paste0("~/MITUS/inst/",loc,"/calibration_outputs/",loc,"_TBdeaths_",simp.date,".rds"))
+##total mortality
+# for (st in 1:51){
+# data("stateID",package="MITUS")
+# data("ST_tot_mort",package="MITUS")
+# loc2<-stateID[st,3]
+#
+# x  <- ST_tot_mort[which(ST_tot_mort$State==stateID[st,1]),]
+# y  <- x[,3:4]
+#   saveRDS(y,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_tot_mort_07-03-19.rds"))
+# }
 
 
-}
+
+# #hrcasessm
+# for (st in 1:51){
+# x<-SCD[["hr_cases_sm"]]
+# hrsm<-matrix(NA,4,ncol(SCD[["hr_cases_sm"]]))
+# loc2<-stateID[st,3]
+# # for (i in 1:4){
+#   y1<-x[((51*(0))+st),]
+#   y2<-x[((51*(1))+st),]
+#   y3<-x[((51*(2))+st),]
+#   y4<-x[((51*(3))+st),]
+#   y<-rbind(y1,y2,y3,y4)
+#   saveRDS(y,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_hr_cases_sm_07-03-19.rds"))
+#
+# # }
+# }
+# #recent fb case
+# for (i in 1:51){
+#   x<-SCD[["rt_fb_cases"]]
+#   rtfb<-matrix(NA,18,ncol(SCD[["rt_fb_cases"]]))
+#   loc2<-stateID[i,3]
+#   y<-dplyr::filter(x,st==loc2)
+#   yy<-cbind(y[,2],y[,7],y[,3])
+#   colnames(yy)<-c("year","pct_fb_rec","sample_size")
+#   saveRDS(yy,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_fb_recent_cases_07-03-19.rds"))
+# }
+
+# #total tb cases
+# for (i in 1:51){
+#   loc2<-stateID[i,3]
+#   x<-SCD[["cases_yr_st"]][[i]]
+#   x[,2]<-x[,2]/1e6
+#   saveRDS(x,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_cases_yr_07-03-19.rds"))
+# }
+
+# #total age tb cases
+# for (i in 1:51){
+#   loc2<-stateID[i,3]
+#   x<-SCD[["cases_yr_ag_nat_st"]][[i]][,-c(1,12),"usb"]*SCD[["cases_yr_ag_nat_st"]][[i]][,12,"usb"]+
+#      SCD[["cases_yr_ag_nat_st"]][[i]][,-c(1,12),"nusb"]*SCD[["cases_yr_ag_nat_st"]][[i]][,12,"nusb"]
+#   y<-cbind(SCD[["cases_yr_ag_nat_st"]][[i]][,1,"usb"],x/rowSums(x),rowSums(x))
+#   saveRDS(y,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_age_cases_tot_07-03-19.rds"))
+# }
+
+
+#
+# #tbdeathsstyr
+# for (i in 1:51){
+#   x<-CalibDatState$tbdeaths[[i]]
+#   loc2<-stateID[i,3]
+#   y<-dplyr::filter(x,State==stateID[i,1])
+#   dist<-matrix(unlist(CalibDatState[["tbdeaths_age_yr"]]),18,11)
+#   z<-yy<-matrix(NA,18,10)
+#   for(i in 1:18){
+#   yy[i,]<-(dist[i,2:11]/sum(dist[i,2:11]))
+#   z[i,]<-x[i,3]*yy[i,]
+#   }
+#   z<-(cbind(x[,2],z))
+#   saveRDS(z,file = paste0("~/MITUS/inst/",loc2,"/calibration_targets/",loc2,"_tb_deaths_07-03-19.rds"))
+#
+#   # }
+# }
+#
+# ##make new data directories
+# # for (i in 1:length(stateID[,3])){
+# #   st<-stateID[i,3]
+# #   newdir<-paste0("~/MITUS/inst/",st,"/calibration_targets")
+# #   dir.create(newdir)
+# # }
