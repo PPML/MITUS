e93b3dd3b8ebb088a8074e8ebbda78f6219a359d
nswartwo <nswartwo@gmail.com>
2019-11-06 10:17:09 -0500

    new state optims based on 1031 national optim

--- R/func_optim_data.R
+++ R/func_optim_data.R
@@ -2,12 +2,13 @@
 #'StartValues Rdata file.
 #'@name optim_data
 #'@param batches dataframe or matrix of starting values data frame
+#'@param loc two digit location code
+#'@param date MM-DD date on optim files
 #'@return datafile of all the optimized data parameters
 #'@export
 
 
-optim_data <- function(batches){
-
+optim_data <- function(batches, loc, date){
 opt_all<-matrix(NA,batches,(nrow(ParamInitZ)+1))
 cnames<-c(rownames(ParamInitZ), "post_val")
 colnames(opt_all)<-cnames
@@ -16,14 +17,34 @@ for (j in 1:batches){
 rnames[j]<-paste("b_no_", j, sep="")
 }
 rownames(opt_all)<-rnames
-
+month<-strsplit(date, "-")[[1]][1]
+day<-strsplit(date, "-")[[1]][2]
 for (i in 1:batches){
-  load(paste("/Users/nis100/Desktop/US_1031/Opt_US_r7_",i,"_2019-10-31.rda", sep=""))
+  load(paste("/Users/nis100/Desktop/", loc, "_", month, day,"/Opt_", loc, "_r7_",i,"_2019-", date, ".rda", sep=""))
   opt_all[i,1:nrow(ParamInitZ)] <- o7$par
   opt_all[i,nrow(ParamInitZ)+1]<- o7$value
 }
-US_opt_all<-opt_all
-saveRDS(US_opt_all, file=paste("~/MITUS/inst/US/US_Optim_all_", batches,"_1031.rds", sep = ""))
+saveRDS(opt_all, file=paste("~/MITUS/inst/", loc,"/", loc, "_Optim_all_", batches,"_", month, day,".rds", sep = ""))
 
 }
 
+optim_data_locs<-function(locs, date){
+  for (loc in locs){
+    model_load(loc)
+    optim_data(10,loc, date)
+  }
+}
+
+##check optim plots for locs
+calib_plots_locs<-function(locs, simp.date, batches=10){
+  for (loc in locs){
+    model_load(loc)
+    Opt<-readRDS(system.file(paste0(loc,"/", loc, "_Optim_all_", batches,"_", simp.date,".rds"), package="MITUS"))
+    posterior<-round(Opt[,ncol(Opt)],2)
+    mode<-round(getmode(posterior), 2)
+    if (mode>1e11){ print(paste(loc, "did not optimize. Check optim manually", sep = " ")); next }
+    samp.i<-sample(which(posterior==mode), 1)
+    Opt<-Opt[,-ncol(Opt)]
+    calib(samp.i, Opt, loc)
+  }
+}
