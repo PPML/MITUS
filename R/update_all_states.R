4ff5c381c720b479d978d9cf01062326ccbe1fe8
nswartwo <nswartwo@gmail.com>
2019-11-21 12:46:31 -0500

    adds a print loc for status checking

--- R/update_all_states.R
+++ R/update_all_states.R
@@ -3,6 +3,7 @@ vector_all_locs<-c("US","CA", "MA", "FL", "GA", "IL", "TX", "NY", "NJ", "PA", "W
 update_all_states<-function(loc_vec, simp.date){
   for (i in 1:length(loc_vec)){
     loc<-loc_vec[i]
+    print(loc)
     model_load(loc)
     optim_to_tabby2(loc, simp.date)
   }
