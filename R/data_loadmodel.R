15d0e108cc09cd285ed31febb17d411495ebb7b9
nswartwo <nswartwo@gmail.com>
2020-05-11 01:16:03 -0400

    new calibdat with smoothed recent NUSB cases

--- R/data_loadmodel.R
+++ R/data_loadmodel.R
@@ -14,7 +14,7 @@ model_load<-function(loc="US"){
 #'load necessary datasets
 #'Model Input
 if (loc=="US"){
-  CalibDat<<-readRDS(system.file("US/US_CalibDat_04-13-20.rds", package="MITUS"))
+  CalibDat<<-readRDS(system.file("US/US_CalibDat_05-10-20.rds", package="MITUS"))
   ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2020-04-28.rds", package="MITUS")))
   StartVal<<-readRDS(system.file("US/US_StartVal_2020-04-28.rds", package="MITUS"))
   Inputs<<-readRDS(system.file("US/US_Inputs_06-26-19.rds", package="MITUS"))
