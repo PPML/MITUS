e5243f44b17d36792a2a09ed7a8263295f496593
nswartwo <nswartwo@gmail.com>
2020-08-12 00:09:01 -0400

    fixes typo in previous commit

--- R/data_loadmodel.R
+++ R/data_loadmodel.R
@@ -15,8 +15,8 @@ model_load<-function(loc="US"){
 #'Model Input
 if (loc=="US"){
   CalibDat<<-readRDS(system.file("US/US_CalibDat_07-27-20.rds", package="MITUS"))
-  ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2020-07-30.rds", package="MITUS")))
-  StartVal<<-readRDS(system.file("US/US_StartVal_2020-07-30.rds", package="MITUS"))
+  ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2020-08-12.rds", package="MITUS")))
+  StartVal<<-readRDS(system.file("US/US_StartVal_2020-08-12.rds", package="MITUS"))
   Inputs<<-readRDS(system.file("US/US_Inputs_08-11-20.rds", package="MITUS"))
 
   # Inputs<<-readRDS(system.file("US/US_Inputs_06-26-19.rds", package="MITUS"))
@@ -25,8 +25,8 @@ if (loc=="US"){
    Opt<<-readRDS(system.file("US/US_Optim_all_10_0709.rds", package="MITUS"))
 } else {
   CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_07-08-20.rds", package="MITUS"))
-  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2020-08-12.rds", package="MITUS"))
-  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2020-08-12.rds", package="MITUS"))
+  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2020-07-31.rds", package="MITUS"))
+  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2020-07-31.rds", package="MITUS"))
   Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_06-09-20.rds"), package="MITUS"))
 }
   if (loc=="IN"){
