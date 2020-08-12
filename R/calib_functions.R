1cae8b09f2dfb137b3d5acfb302063d0dbad3631
nswartwo <nswartwo@gmail.com>
2020-08-12 00:15:26 -0400

    undoes forcing of RT likelihood

--- R/calib_functions.R
+++ R/calib_functions.R
@@ -45,7 +45,7 @@ notif_us_lik <- function(V) {
 recent_trans_dist_lLik  <- function(V) {
   rct_trans_dist        <- CalibDat[["recent_trans_cases"]][1:2,1]/CalibDat[["recent_trans_cases"]][1:2,3]
   adj_13          <- sum(dbeta(rct_trans_dist,rct_trans_dist*100,(1-rct_trans_dist)*100,log=T) )
-  (sum(dbeta(rct_trans_dist,V[,1]*100,V[,2]*100,log=T) ) - adj_13)*25
+  (sum(dbeta(rct_trans_dist,V[,1]*100,V[,2]*100,log=T) ) - adj_13)
   }
 
 #' CASES FB DISTRIBUTION 1993-2014
