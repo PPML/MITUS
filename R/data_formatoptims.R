d3b35e1900f7f55c06fc75d5e555f96548a741b4
nswartwo <nswartwo@gmail.com>
2020-11-17 15:44:49 -0500

    new optims for states pt 1

--- R/data_formatoptims.R
+++ R/data_formatoptims.R
@@ -19,7 +19,7 @@ rownames(opt_all)<-rnames
 month<-strsplit(date, "-")[[1]][1]
 day<-strsplit(date, "-")[[1]][2]
 for (i in batches){
-  load(paste("/Users/nis100/Desktop/states_110820/","Opt_", loc, "_r9_",i,"_2020-", date, ".rda", sep=""))
+  load(paste("/Users/nis100/Desktop/states_111620/","Opt_", loc, "_r9_",i,"_2020-", date, ".rda", sep=""))
   opt_all[i,1:nrow(ParamInitZ)] <- o9$par
   opt_all[i,nrow(ParamInitZ)+1]<- o9$value
 }
