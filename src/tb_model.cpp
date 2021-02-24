d6650ad6a3152b23f478c98b2466f9b4ace1edda
nswartwo <nswartwo@gmail.com>
2021-02-24 11:35:23 -0500

    fixes ExogInf index in burn-in

--- src/tb_model.cpp
+++ src/tb_model.cpp
@@ -555,14 +555,14 @@ Rcpp::List cSim(
         ///////// HIGH RISK US BORN
         VLkla[1 ][0 ][ag]  = RelInfRg[1] * RRcrAG[ag] * (Vjaf[1]*(1-Vmix[0]) + Vjaf[0]*Vmix[0]);
         ///////// LOW RISK NON US BORN
-        VLkla[0 ][1 ][ag]  = RelInfRg[2] * RRcrAG[ag] * (Vjaf[2]*(1-Vmix[1]) + Vjaf[0]*Vmix[1]) + ExogInf[s];
+        VLkla[0 ][1 ][ag]  = RelInfRg[2] * RRcrAG[ag] * (Vjaf[2]*(1-Vmix[1]) + Vjaf[0]*Vmix[1]) + ExogInf[0];
         ///////// HIGH RISK NON US BORN
         ///check the use of RelInfRg here as beta, might need to be a combo param but unclear check the old param file
         VLkla[1 ][1 ][ag]  = RelInfRg[3] * RRcrAG[ag] *
           (Vjaf[3] * (1-Vmix[0]) * (1-Vmix[1]) +
           Vjaf[2] *    Vmix[0]  * (1-Vmix[1]) +
           Vjaf[1] * (1-Vmix[0]) *    Vmix[1]  +
-          Vjaf[0] *    Vmix[0]  *    Vmix[1])  + ExogInf[s];
+          Vjaf[0] *    Vmix[0]  *    Vmix[1])  + ExogInf[0];
       }      ///////////////////////////////INFECTION///////////////////////////////////////
       ///////////////////////for all age groups, risk groups/////////////////////////
       ///////INFECTION IS CALCULATED WITH THE FORCE OF INFECTION BY RISK GROUP///////
