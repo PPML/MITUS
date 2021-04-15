a779acfd617796ea54ec6a1f17a4a867bbcf7fa2
nswartwo <nswartwo@gmail.com>
2021-04-14 23:30:33 -0400

    incorporates a spline for TB burden among migrants

--- R/data_loadmodel.R
+++ R/data_loadmodel.R
@@ -15,20 +15,19 @@ model_load<-function(loc="US"){
 #'Model Input
 if (loc=="US"){
   CalibDat<<-readRDS(system.file("US/US_CalibDat_2021-01-28.rds", package="MITUS"))
-  ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2020-08-20.rds", package="MITUS")))
-  StartVal<<-readRDS(system.file("US/US_StartVal_2021-03-02.rds", package="MITUS"))
+  # ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2020-08-20.rds", package="MITUS")))
+  ParamInit<<-readRDS(system.file("US/US_ParamInit_2021-04-14.rds", package="MITUS"))
+  # StartVal<<-readRDS(system.file("US/US_StartVal_2021-03-02.rds", package="MITUS"))
+  StartVal<<-readRDS(system.file("US/US_StartVal_2021-04-14.rds", package="MITUS"))
+
   Inputs<<-readRDS(system.file("US/US_Inputs_08-31-20.rds", package="MITUS"))
-  # Inputs<<-readRDS(system.file("US/US_Inputs_06-26-19.rds", package="MITUS"))
-  # TabOpt<<-readRDS(system.file("US/US_Optim_all_9_0609.rds", package="MITUS"))
-   # Opt<<-readRDS(system.file("US/US_Optim_all_10_0709.rds", package="MITUS"))
-  # Opt <- readRDS(system.file("US/US_Optim_all_10_1201.rds", package="MITUS"))
 
 } else {
   CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_2020-12-03.rds", package="MITUS"))
-  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2021-03-04.rds", package="MITUS"))
-  # ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2020-07-31.rds", package="MITUS"))
+  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2021-04-14.rds", package="MITUS"))
+  # StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2021-03-04.rds", package="MITUS"))
+  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2021-04-14.rds", package="MITUS"))
 
-  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2021-03-04.rds", package="MITUS"))
   Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_10-19-20.rds"), package="MITUS"))
   #last input change was to update the RR active TB by age in immigrants
 }
