badf2d6c56f8b68430dd1ea03221b71ea57943f1
nswartwo <nswartwo@gmail.com>
2021-07-19 23:37:31 -0400

    new data based on 7/19 national optimization

--- R/data_loadmodel.R
+++ R/data_loadmodel.R
@@ -18,25 +18,15 @@ if (loc=="US"){
   ParamInit<<-as.data.frame(readRDS(system.file("US/US_ParamInit_2021-06-29.rds", package="MITUS")))
   StartVal<<-readRDS(system.file("US/US_StartVal_2021-06-29.rds", package="MITUS"))
   Inputs<<-readRDS(system.file("US/US_Inputs_08-31-20.rds", package="MITUS"))
-  #Opt <<- readRDS(system.file("US/US_Optim_all_10_0709.rds", package="MITUS"))
-  #Par <<- readRDS(system.file("US/US_Param_all_10_0709.rds", package="MITUS"))
-
-  # Inputs<<-readRDS(system.file("US/US_Inputs_06-26-19.rds", package="MITUS"))
-  # TabOpt<<-readRDS(system.file("US/US_Optim_all_9_0609.rds", package="MITUS"))
-   # Opt<<-readRDS(system.file("US/US_Optim_all_10_0709.rds", package="MITUS"))
-
+  Opt <<- readRDS(system.file("US/US_Optim_all_10_0719.rds", package="MITUS"))
+  Par <<- readRDS(system.file("US/US_Param_all_10_0719.rds", package="MITUS"))
 } else {
   CalibDat<<-CalibDatState<<-readRDS(system.file("ST/ST_CalibDat_2020-12-03.rds", package="MITUS"))
-  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2021-03-04.rds", package="MITUS"))
-  # ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2020-07-31.rds", package="MITUS"))
-
-  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2021-03-04.rds", package="MITUS"))
+  ParamInit_st<<-ParamInit<<-readRDS(system.file("ST/ST_ParamInit_2021-07-19.rds", package="MITUS"))
+  StartVal_st<<-StartVal<<-readRDS(system.file("ST/ST_StartVal_2021-07-19.rds", package="MITUS"))
   Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_10-19-20.rds"), package="MITUS"))
   #last input change was to update the RR active TB by age in immigrants
 }
-  # if (loc=="IN"){
-  #   Inputs<<-readRDS(system.file(paste0(loc,"/",loc,"_ModelInputs_07-14-20.rds"), package="MITUS"))
-  # }
 if (loc=="US"){
   wts <<- CalibDat[["ImptWeights"]]
   P  <<- ParamInit[,1]
