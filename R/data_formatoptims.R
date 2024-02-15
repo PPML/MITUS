ca011ab2c0fef5d45ea1d4881fd4c7a027aefab6
nswartwo <nswartwo@gmail.com>
2024-02-15 18:25:02 -0500

    updates year to 2024

--- R/data_formatoptims.R
+++ R/data_formatoptims.R
@@ -19,7 +19,7 @@ optim_data <- function(batches, loc, date){
   month<-strsplit(date, "-")[[1]][1]
   day<-strsplit(date, "-")[[1]][2]
   for (i in batches){
-    load(paste("/Users/nis100/Desktop/ModelOpts_122123/","Opt_", loc, "_r7_",i,"_2023-", date, ".rda", sep=""))
+    load(paste("/Users/nis100/Desktop/ModelOpts_011924/","Opt_", loc, "_r7_",i,"_2024-", date, ".rda", sep=""))
     opt_all[i,1:nrow(ParamInitZ)] <- o7$par
     opt_all[i,nrow(ParamInitZ)+1]<- o7$value
   }
@@ -37,22 +37,22 @@ optim_data_locs<-function(locs, date){
 # all_optim <- data.frame("State" = ordered_locs, "Sample" = rep(0,51))
 
 calib_plots_locs<-function(locs, simp.date, batches=15){
-   for (loc in locs){
-      print(loc)
-      model_load(loc)
-      Opt<-readRDS(system.file(paste0(loc,"/", loc, "_Optim_all_", batches,"_", simp.date,".rds"), package="MITUS"))
-      posterior<-rep(0,15)
+  for (loc in locs){
+    print(loc)
+    model_load(loc)
+    Opt<-readRDS(system.file(paste0(loc,"/", loc, "_Optim_all_", batches,"_", simp.date,".rds"), package="MITUS"))
+    posterior<-rep(0,15)
 
-      for(i in 1:15){ posterior[i]<-trunc_number_n_decimals(Opt[i,ncol(Opt)],1);  print(posterior[i]) }
-      # mode<-round(getmode(posterior), 2); print(mode)
-      # if (mode>1e11){ print(paste(loc, "did not optimize. Check optim manually", sep = " ")); next }
-      samp.i<-which(posterior==min(posterior));print(samp.i)
-      Opt<-Opt[,-ncol(Opt)]
-      results <-calib(samp.i[1], Opt, loc, cex.size = .8)
-      ### Create Calibration Targets
-      model_calib_outputs(loc, results, 1, simp.date)
-      # model_calib_outputs_2020(loc, results, 1, simp.date)
-   }}
+    for(i in 1:15){ posterior[i]<-trunc_number_n_decimals(Opt[i,ncol(Opt)],1);  print(posterior[i]) }
+    # mode<-round(getmode(posterior), 2); print(mode)
+    # if (mode>1e11){ print(paste(loc, "did not optimize. Check optim manually", sep = " ")); next }
+    samp.i<-which(posterior==min(posterior));print(samp.i)
+    Opt<-Opt[,-ncol(Opt)]
+    results <-calib(samp.i[1], Opt, loc, cex.size = .8)
+    ### Create Calibration Targets
+    model_calib_outputs(loc, results, 1, simp.date)
+    # model_calib_outputs_2020(loc, results, 1, simp.date)
+  }}
 
 calib_plots_locs_2020<-function(locs, simp.date, batches=15){
   for (loc in locs){
